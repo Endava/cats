3ba24ce9492cbbed7e795052120329b016e56207
Madalin Ilie <madalin.ilie@endava.com>
2020-07-22 13:27:07 +0300

    Replace generex library with rgxgen
    It seems that generex often reaches stackoverflow errors when encountering different patterns.
    Switching to rgxgen as it seems more appropiate for now.

--- src/main/java/com/endava/cats/generator/simple/StringGenerator.java
+++ src/main/java/com/endava/cats/generator/simple/StringGenerator.java
@@ -1,13 +1,13 @@
 package com.endava.cats.generator.simple;
 
-import com.mifmif.common.regex.Generex;
+import com.github.curiousoddman.rgxgen.RgxGen;
 import io.swagger.v3.oas.models.media.Schema;
 import org.apache.commons.lang3.StringUtils;
 
 public class StringGenerator {
     public static final String FUZZ = "fuzz";
     public static final int DEFAULT_MAX_LENGTH = 2000;
-    private static final String ALPHANUMERIC = "[a-zA-Z0-9]*";
+    public static final String ALPHANUMERIC = "[a-zA-Z0-9]";
 
     private StringGenerator() {
         //ntd
@@ -21,12 +21,17 @@ public class StringGenerator {
         return StringUtils.repeat(FUZZ, times);
     }
 
-    public static String generateRightBoundString(Schema schema) {
-        Generex generex = new Generex(ALPHANUMERIC);
+    public static String generate(String pattern, int min, int max) {
+        String completePattern = sanitize(pattern) + "{" + min + "," + max + "}";
 
+        return new RgxGen(completePattern).generate();
+    }
+
+    public static String generateRightBoundString(Schema schema) {
         int minLength = schema.getMaxLength() != null ? schema.getMaxLength() + 10 : DEFAULT_MAX_LENGTH;
 
-        return generex.random(minLength);
+        String pattern = ALPHANUMERIC + "{" + minLength + ",}";
+        return new RgxGen(pattern).generate();
     }
 
     public static String generateLeftBoundString(Schema schema) {
@@ -35,9 +40,9 @@ public class StringGenerator {
         if (minLength <= 0) {
             return "";
         }
-        Generex generex = new Generex(ALPHANUMERIC);
+        String pattern = ALPHANUMERIC + "{" + (minLength - 1) + "," + minLength + "}";
 
-        return generex.random(minLength - 1, minLength);
+        return new RgxGen(pattern).generate();
     }
 
     public static String sanitize(String pattern) {
