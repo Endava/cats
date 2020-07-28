c9d4aa770efa7984e57950ed8c67ce61f993cd67
Madalin Ilie <madalin.ilie@endava.com>
2020-07-28 16:15:29 +0300

    Increase test coverage

--- src/test/java/com/endava/cats/io/ServiceCallerTest.java
+++ src/test/java/com/endava/cats/io/ServiceCallerTest.java
@@ -36,7 +36,11 @@ class ServiceCallerTest {
         wireMockServer = new WireMockServer(PORT);
         wireMockServer.start();
         wireMockServer.stubFor(WireMock.post("/pets").willReturn(WireMock.aResponse().withBody("{'result':'OK'}")));
+        wireMockServer.stubFor(WireMock.put("/pets").willReturn(WireMock.aResponse().withBody("{'result':'OK'}")));
         wireMockServer.stubFor(WireMock.get("/pets/1").willReturn(WireMock.aResponse().withBody("{'pet':'pet'}")));
+        wireMockServer.stubFor(WireMock.delete("/pets/1").willReturn(WireMock.aResponse()));
+        wireMockServer.stubFor(WireMock.head(WireMock.urlEqualTo("/pets/1")).willReturn(WireMock.aResponse()));
+
     }
 
     @AfterAll
@@ -45,12 +49,9 @@ class ServiceCallerTest {
     }
 
     @BeforeEach
-    void setupServiceCaller() {
+    public void setupEach() throws Exception {
         serviceCaller = new ServiceCaller(testCaseListener, catsUtil);
-    }
 
-    @Test
-    void givenAServer_whenDoingAPostCall_thenProperDetailsAreBeingReturned() throws Exception {
         ReflectionTestUtils.setField(serviceCaller, "server", "http://localhost:" + PORT);
         ReflectionTestUtils.setField(serviceCaller, "refDataFile", "src/test/resources/refFields.yml");
         ReflectionTestUtils.setField(serviceCaller, "headersFile", "src/test/resources/headers.yml");
@@ -61,26 +62,38 @@ class ServiceCallerTest {
 
         Mockito.doCallRealMethod().when(catsUtil).parseAsJsonElement(Mockito.anyString());
         Mockito.doCallRealMethod().when(catsUtil).isValidJson(Mockito.anyString());
+    }
+
+    @Test
+    void givenAServer_whenDoingADeleteCall_thenProperDetailsAreBeingReturned() {
+        CatsResponse catsResponse = serviceCaller.call(HttpMethod.DELETE, ServiceData.builder().relativePath("/pets/{id}").payload("{'id':'1'}")
+                .headers(Collections.singleton(CatsHeader.builder().name("header").value("header").build())).build());
+
+        Assertions.assertThat(catsResponse.responseCodeAsString()).isEqualTo("200");
+        Assertions.assertThat(catsResponse.getBody()).isEmpty();
+
+    }
+
+    @Test
+    void givenAServer_whenDoingAPostCall_thenProperDetailsAreBeingReturned() {
         CatsResponse catsResponse = serviceCaller.call(HttpMethod.POST, ServiceData.builder().relativePath("/pets").payload("{'field':'oldValue'}")
                 .headers(Collections.singleton(CatsHeader.builder().name("header").value("header").build())).build());
 
         Assertions.assertThat(catsResponse.responseCodeAsString()).isEqualTo("200");
         Assertions.assertThat(catsResponse.getBody()).isEqualTo("{'result':'OK'}");
-
     }
 
     @Test
-    void givenAServer_whenDoingAGetCall_thenProperDetailsAreBeingReturned() throws Exception {
-        ReflectionTestUtils.setField(serviceCaller, "server", "http://localhost:" + PORT);
-        ReflectionTestUtils.setField(serviceCaller, "refDataFile", "src/test/resources/refFields.yml");
-        ReflectionTestUtils.setField(serviceCaller, "headersFile", "src/test/resources/headers.yml");
-        Mockito.doCallRealMethod().when(catsUtil).parseYaml(Mockito.any());
+    void givenAServer_whenDoingAPutCall_thenProperDetailsAreBeingReturned() {
+        CatsResponse catsResponse = serviceCaller.call(HttpMethod.PUT, ServiceData.builder().relativePath("/pets").payload("{'field':'newValue'}")
+                .headers(Collections.singleton(CatsHeader.builder().name("header").value("header").build())).build());
 
-        serviceCaller.loadHeaders();
-        serviceCaller.loadRefData();
+        Assertions.assertThat(catsResponse.responseCodeAsString()).isEqualTo("200");
+        Assertions.assertThat(catsResponse.getBody()).isEqualTo("{'result':'OK'}");
+    }
 
-        Mockito.doCallRealMethod().when(catsUtil).parseAsJsonElement(Mockito.anyString());
-        Mockito.doCallRealMethod().when(catsUtil).isValidJson(Mockito.anyString());
+    @Test
+    void givenAServer_whenDoingAGetCall_thenProperDetailsAreBeingReturned() {
         CatsResponse catsResponse = serviceCaller.call(HttpMethod.GET, ServiceData.builder().relativePath("/pets/{id}").payload("{'id':'1'}")
                 .headers(Collections.singleton(CatsHeader.builder().name("header").value("header").build())).build());
 
