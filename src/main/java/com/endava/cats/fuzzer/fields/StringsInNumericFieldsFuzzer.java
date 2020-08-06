548c23e32f024ea41d02863ae15ac488e1283322
Madalin Ilie <madalin.ilie@endava.com>
2020-08-06 20:38:17 +0300

    Add new StringsInNumericFields fuzzer

---
+++ src/main/java/com/endava/cats/fuzzer/fields/StringsInNumericFieldsFuzzer.java
@@ -0,0 +1,48 @@
+package com.endava.cats.fuzzer.fields;
+
+import com.endava.cats.generator.simple.StringGenerator;
+import com.endava.cats.io.ServiceCaller;
+import com.endava.cats.report.TestCaseListener;
+import com.endava.cats.util.CatsUtil;
+import io.swagger.v3.oas.models.media.IntegerSchema;
+import io.swagger.v3.oas.models.media.NumberSchema;
+import io.swagger.v3.oas.models.media.Schema;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class StringsInNumericFieldsFuzzer extends BaseBoundaryFieldFuzzer {
+
+    @Autowired
+    public StringsInNumericFieldsFuzzer(ServiceCaller sc, TestCaseListener lr, CatsUtil cu) {
+        super(sc, lr, cu);
+    }
+
+    @Override
+    protected List<Class<? extends Schema>> getSchemasThatTheFuzzerWillApplyTo() {
+        return Arrays.asList(NumberSchema.class, IntegerSchema.class);
+    }
+
+    @Override
+    protected String getBoundaryValue(Schema schema) {
+        return StringGenerator.generateRandomString();
+    }
+
+    @Override
+    protected boolean hasBoundaryDefined(Schema schema) {
+        return true;
+    }
+
+    @Override
+    protected String typeOfDataSentToTheService() {
+        return "strings in numeric fields";
+    }
+
+    @Override
+    public String description() {
+        return "iterate through each Integer (int, long) and Number field (float, double) and send requests with alphanumeric characters in the targeted field";
+    }
+}
