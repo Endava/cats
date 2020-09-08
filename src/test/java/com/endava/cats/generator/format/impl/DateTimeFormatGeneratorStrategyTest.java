4d02d5514319169a3f17db97a01d2f1f4bc46cde
Madalin Ilie <madalin.ilie@endava.com>
2020-09-08 16:52:00 +0300

    Fix for the way dates were generated

--- src/test/java/com/endava/cats/generator/format/impl/DateTimeFormatGeneratorStrategyTest.java
+++ src/test/java/com/endava/cats/generator/format/impl/DateTimeFormatGeneratorStrategyTest.java
@@ -10,13 +10,13 @@ class DateTimeFormatGeneratorStrategyTest {
     @Test
     void givenADateTimeFormatGeneratorStrategy_whenGettingTheAlmostValidValue_thenTheValueIsReturnedAsExpected() {
         DateTimeFormatGeneratorStrategy strategy = new DateTimeFormatGeneratorStrategy();
-        Assertions.assertThat(strategy.getAlmostValidValue()).isEqualTo(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'").format(LocalDateTime.now()));
+        Assertions.assertThat(strategy.getAlmostValidValue()).isEqualTo(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:'Z'").format(LocalDateTime.now()));
     }
 
 
     @Test
     void givenADateTimeFormatGeneratorStrategy_whenGettingTheTotallyWrongValue_thenTheValueIsReturnedAsExpected() {
         DateTimeFormatGeneratorStrategy strategy = new DateTimeFormatGeneratorStrategy();
-        Assertions.assertThat(strategy.getTotallyWrongValue()).isEqualTo("1000-07-21T17:32:28Z");
+        Assertions.assertThat(strategy.getTotallyWrongValue()).isEqualTo("1000-07-21T88:32:28Z");
     }
 }
