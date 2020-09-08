ea3f42b7c4bed5c6606eed05bf78f78adeb5a613
Madalin Ilie <madalin.ilie@endava.com>
2020-09-08 16:52:39 +0300

    Fix for arrays that can be set as query params

--- src/test/java/com/endava/cats/model/ann/ExcludeTestCaseStrategyTest.java
+++ src/test/java/com/endava/cats/model/ann/ExcludeTestCaseStrategyTest.java
@@ -5,7 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ExcludeTestCaseStrategyTest {
+class ExcludeTestCaseStrategyTest {
 
     private ExcludeTestCaseStrategy excludeTestCaseStrategy;
 
