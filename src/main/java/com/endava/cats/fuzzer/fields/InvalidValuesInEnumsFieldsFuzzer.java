608d8586ea1620c59f91aa56e2a551d68975f061
Madalin Ilie <madalin.ilie@endava.com>
2020-09-14 17:58:29 +0300

    Add new Fuzzer for invalid values in ENUMs

---
+++ src/main/java/com/endava/cats/fuzzer/fields/InvalidValuesInEnumsFieldsFuzzer.java
@@ -0,0 +1,49 @@
+package com.endava.cats.fuzzer.fields;
+
+import com.endava.cats.generator.simple.StringGenerator;
+import com.endava.cats.io.ServiceCaller;
+import com.endava.cats.report.TestCaseListener;
+import com.endava.cats.util.CatsUtil;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.media.StringSchema;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class InvalidValuesInEnumsFieldsFuzzer extends BaseBoundaryFieldFuzzer {
+
+    public InvalidValuesInEnumsFieldsFuzzer(ServiceCaller sc, TestCaseListener lr, CatsUtil cu) {
+        super(sc, lr, cu);
+    }
+
+    @Override
+    protected String typeOfDataSentToTheService() {
+        return "invalid ENUM values";
+    }
+
+    @Override
+    protected List<Class<? extends Schema>> getSchemasThatTheFuzzerWillApplyTo() {
+        return Collections.singletonList(StringSchema.class);
+    }
+
+    @Override
+    protected String getBoundaryValue(Schema schema) {
+        if (schema.getEnum() != null) {
+            int length = String.valueOf(schema.getEnum().get(0)).length();
+            return StringGenerator.generate("[A-Z]+", length, length);
+        }
+        return "";
+    }
+
+    @Override
+    protected boolean hasBoundaryDefined(Schema schema) {
+        return schema.getEnum() != null;
+    }
+
+    @Override
+    public String description() {
+        return "iterate through each ENUM field and send invalid values";
+    }
+}
---
+++ src/test/java/com/endava/cats/fuzzer/fields/InvalidValuesInEnumsFieldsFuzzerTest.java
@@ -0,0 +1,52 @@
+package com.endava.cats.fuzzer.fields;
+
+import com.endava.cats.io.ServiceCaller;
+import com.endava.cats.report.TestCaseListener;
+import com.endava.cats.util.CatsUtil;
+import io.swagger.v3.oas.models.media.StringSchema;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Collections;
+
+@ExtendWith(SpringExtension.class)
+class InvalidValuesInEnumsFieldsFuzzerTest {
+
+    @Mock
+    private ServiceCaller serviceCaller;
+    @Mock
+    private TestCaseListener testCaseListener;
+    @Mock
+    private CatsUtil catsUtil;
+    private InvalidValuesInEnumsFieldsFuzzer invalidValuesInEnumsFieldsFuzzer;
+
+    @BeforeEach
+    void setup() {
+        invalidValuesInEnumsFieldsFuzzer = new InvalidValuesInEnumsFieldsFuzzer(serviceCaller, testCaseListener, catsUtil);
+    }
+
+    @Test
+    void shouldNotHaveBoundaryDefined() {
+        StringSchema stringSchema = new StringSchema();
+        Assertions.assertThat(invalidValuesInEnumsFieldsFuzzer.getSchemasThatTheFuzzerWillApplyTo().stream().anyMatch(schema -> schema.isAssignableFrom(StringSchema.class))).isTrue();
+        Assertions.assertThat(invalidValuesInEnumsFieldsFuzzer.getBoundaryValue(stringSchema)).isEmpty();
+        Assertions.assertThat(invalidValuesInEnumsFieldsFuzzer.hasBoundaryDefined(stringSchema)).isFalse();
+        Assertions.assertThat(invalidValuesInEnumsFieldsFuzzer.description()).isNotNull();
+        Assertions.assertThat(invalidValuesInEnumsFieldsFuzzer.typeOfDataSentToTheService()).isNotNull();
+    }
+
+    @Test
+    void shouldHaveBoundaryDefined() {
+        StringSchema stringSchema = new StringSchema();
+        stringSchema.setEnum(Collections.singletonList("TEST"));
+        Assertions.assertThat(invalidValuesInEnumsFieldsFuzzer.getSchemasThatTheFuzzerWillApplyTo().stream().anyMatch(schema -> schema.isAssignableFrom(StringSchema.class))).isTrue();
+        Assertions.assertThat(invalidValuesInEnumsFieldsFuzzer.getBoundaryValue(stringSchema)).hasSizeLessThanOrEqualTo(4);
+        Assertions.assertThat(invalidValuesInEnumsFieldsFuzzer.hasBoundaryDefined(stringSchema)).isTrue();
+        Assertions.assertThat(invalidValuesInEnumsFieldsFuzzer.description()).isNotNull();
+        Assertions.assertThat(invalidValuesInEnumsFieldsFuzzer.typeOfDataSentToTheService()).isNotNull();
+    }
+}
