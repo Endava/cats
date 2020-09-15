bc40ebdafe5a723a9914d9d852171db7a7afed8a
Madalin Ilie <madalin.ilie@endava.com>
2020-09-15 17:07:06 +0300

    Make CustomFuzzer also consider the httpMethod for a given custom test

--- src/main/java/com/endava/cats/http/HttpMethod.java
+++ src/main/java/com/endava/cats/http/HttpMethod.java
@@ -1,5 +1,14 @@
 package com.endava.cats.http;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 public enum HttpMethod {
-    POST, PUT, GET, TRACE, DELETE, PATCH, HEAD
+    POST, PUT, GET, TRACE, DELETE, PATCH, HEAD;
+
+    public static Optional<HttpMethod> fromString(String httpM) {
+        return Arrays.stream(values())
+                .filter(enV -> enV.name().equalsIgnoreCase(httpM))
+                .findFirst();
+    }
 }
