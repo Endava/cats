1b68b87ab711b5aaa7bbe14b39dd2e9f3679849e
Madalin Ilie <madalin.ilie@endava.com>
2020-10-15 21:43:56 +0300

    Add a new Fuzzer for checking if the versions are part of the contract paths

---
+++ src/main/java/com/endava/cats/fuzzer/contract/VersionsContractInfoFuzzer.java
@@ -0,0 +1,55 @@
+package com.endava.cats.fuzzer.contract;
+
+import com.endava.cats.fuzzer.ContractInfoFuzzer;
+import com.endava.cats.model.FuzzingData;
+import com.endava.cats.report.TestCaseListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@ContractInfoFuzzer
+@Component
+@Slf4j
+public class VersionsContractInfoFuzzer extends BaseContractInfoFuzzer {
+    private static final List<String> VERSIONS = Arrays.asList("version\\d*\\.?", "v\\d+\\.?");
+
+
+    public VersionsContractInfoFuzzer(TestCaseListener tcl) {
+        super(tcl);
+    }
+
+    @Override
+    public void process(FuzzingData data) {
+        if (!fuzzedPaths.contains(data.getPath())) {
+            testCaseListener.addScenario(log, "Scenario: Check if the current path contains versioning information");
+            testCaseListener.addExpectedResult(log, "Paths should not contain versioning information. This should be handled in the [servers] definition");
+            testCaseListener.addPath(data.getPath());
+            testCaseListener.addFullRequestPath("NA");
+            fuzzedPaths.add(data.getPath());
+
+            boolean found = false;
+            for (String version : VERSIONS) {
+                Pattern p = Pattern.compile(version);
+                Matcher m = p.matcher(data.getPath());
+                if (m.find()) {
+                    found = true;
+                }
+            }
+
+            if (found) {
+                testCaseListener.reportError(log, "Path contains versioning information");
+            } else {
+                testCaseListener.reportInfo(log, "Path does not contain versioning information");
+            }
+        }
+    }
+
+    @Override
+    public String description() {
+        return "verifies that a given path doesn't contain versioning information";
+    }
+}
