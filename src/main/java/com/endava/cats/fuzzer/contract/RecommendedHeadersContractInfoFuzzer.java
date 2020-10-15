d37a274ae7230d8d7846beaad02c4f7a4b0e4f23
Madalin Ilie <madalin.ilie@endava.com>
2020-10-15 21:16:31 +0300

    Add a new Fuzzer for checking recommended headers in contract

---
+++ src/main/java/com/endava/cats/fuzzer/contract/RecommendedHeadersContractInfoFuzzer.java
@@ -0,0 +1,52 @@
+package com.endava.cats.fuzzer.contract;
+
+import com.endava.cats.fuzzer.ContractInfoFuzzer;
+import com.endava.cats.model.CatsHeader;
+import com.endava.cats.model.FuzzingData;
+import com.endava.cats.report.TestCaseListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ContractInfoFuzzer
+@Component
+@Slf4j
+public class RecommendedHeadersContractInfoFuzzer extends BaseContractInfoFuzzer {
+    protected static final List<String> HEADERS = Arrays.asList("correlationid", "traceid");
+
+    @Autowired
+    public RecommendedHeadersContractInfoFuzzer(TestCaseListener tcl) {
+        super(tcl);
+    }
+
+    @Override
+    public void process(FuzzingData data) {
+        testCaseListener.addScenario(log, "Scenario: Check if the current path contains recommended headers such as CorrelationId/TraceId");
+        testCaseListener.addExpectedResult(log, "Recommended headers [TraceId/CorrelationId] must be present");
+        testCaseListener.addPath(data.getPath());
+        testCaseListener.addFullRequestPath("NA");
+
+        List<CatsHeader> recommendedHeaders = data.getHeaders().stream()
+                .filter(catsHeader -> HEADERS.parallelStream().anyMatch(this.replaceSpecialChars(catsHeader.getName()).toLowerCase()::contains))
+                .collect(Collectors.toList());
+
+        if (recommendedHeaders.isEmpty()) {
+            testCaseListener.reportError(log, "Path does not contain the recommended [TracedId/CorrelationId] headers for HTTP method {}", data.getMethod());
+        } else {
+            testCaseListener.reportInfo(log, "Path contains the recommended [TracedId/CorrelationId] headers for HTTP method {}", data.getMethod());
+        }
+    }
+
+    private String replaceSpecialChars(String header) {
+        return header.replaceAll("[\\s-_]+", "");
+    }
+
+    @Override
+    public String description() {
+        return "verifies that all OpenAPI contract paths contain recommended headers like: CorrelationId/TraceId, etc. ";
+    }
+}
---
+++ src/test/java/com/endava/cats/fuzzer/contract/RecommendedHeadersContractInfoFuzzerTest.java
@@ -0,0 +1,83 @@
+package com.endava.cats.fuzzer.contract;
+
+import com.endava.cats.http.HttpMethod;
+import com.endava.cats.io.TestCaseExporter;
+import com.endava.cats.model.CatsHeader;
+import com.endava.cats.model.FuzzingData;
+import com.endava.cats.report.ExecutionStatisticsListener;
+import com.endava.cats.report.TestCaseListener;
+import com.google.common.collect.Sets;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mockito;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+class RecommendedHeadersContractInfoFuzzerTest {
+    @SpyBean
+    private TestCaseListener testCaseListener;
+
+    @MockBean
+    private ExecutionStatisticsListener executionStatisticsListener;
+
+    @MockBean
+    private TestCaseExporter testCaseExporter;
+
+    @SpyBean
+    private BuildProperties buildProperties;
+
+    private RecommendedHeadersContractInfoFuzzer recommendedHeadersContractInfoFuzzer;
+
+    @BeforeAll
+    static void init() {
+        System.setProperty("name", "cats");
+        System.setProperty("version", "4.3.2");
+        System.setProperty("time", "100011111");
+    }
+
+    @BeforeEach
+    void setup() {
+        recommendedHeadersContractInfoFuzzer = new RecommendedHeadersContractInfoFuzzer(testCaseListener);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"X-Trace-Id", "XTraceid", "X-CorrelationId", "X-APP-Correlation_Id"})
+    void shouldReportInfo(String header) {
+        CatsHeader catsHeader = CatsHeader.builder().name(header).build();
+        FuzzingData data = FuzzingData.builder().headers(Sets.newHashSet(catsHeader)).method(HttpMethod.POST).build();
+        recommendedHeadersContractInfoFuzzer.fuzz(data);
+
+        Mockito.verify(testCaseListener, Mockito.times(1)).reportInfo(Mockito.any(),
+                Mockito.eq("Path contains the recommended [TracedId/CorrelationId] headers for HTTP method {}"), Mockito.eq(HttpMethod.POST));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"X-Trac-Id", "XTracing", "X-Correlation", "X-APP-Correlation*Id"})
+    void shouldReportError(String header) {
+        CatsHeader catsHeader = CatsHeader.builder().name(header).build();
+        FuzzingData data = FuzzingData.builder().headers(Sets.newHashSet(catsHeader)).method(HttpMethod.POST).build();
+        recommendedHeadersContractInfoFuzzer.fuzz(data);
+
+        Mockito.verify(testCaseListener, Mockito.times(1)).reportError(Mockito.any(),
+                Mockito.eq("Path does not contain the recommended [TracedId/CorrelationId] headers for HTTP method {}"), Mockito.eq(HttpMethod.POST));
+    }
+
+    @Test
+    void shouldReturnSimpleClassNameForToString() {
+        Assertions.assertThat(recommendedHeadersContractInfoFuzzer).hasToString(recommendedHeadersContractInfoFuzzer.getClass().getSimpleName());
+    }
+
+    @Test
+    void shouldReturnMeaningfulDescription() {
+        Assertions.assertThat(recommendedHeadersContractInfoFuzzer.description()).isEqualTo("verifies that all OpenAPI contract paths contain recommended headers like: CorrelationId/TraceId, etc. ");
+    }
+
+}
