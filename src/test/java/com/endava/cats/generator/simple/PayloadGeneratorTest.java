f8e5a4f1ec99202c5c2156e488c78382b39d3f01
Madalin Ilie <madalin.ilie@endava.com>
2020-10-22 20:46:11 +0300

    Fix an issue when an OpenAPI object was only containing allOf elements
    Then there was an object containing only a reference to allOf to another object
    CATS was not preserving the root key, but only adding the elements from the allOf reference.
    Now it properly creates a composed Json object with the initial key and all the allOf properties.

--- src/test/java/com/endava/cats/generator/simple/PayloadGeneratorTest.java
+++ src/test/java/com/endava/cats/generator/simple/PayloadGeneratorTest.java
@@ -8,6 +8,7 @@ import io.swagger.v3.parser.core.models.ParseOptions;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
@@ -18,14 +19,7 @@ class PayloadGeneratorTest {
 
     @Test
     void givenASimpleOpenAPIContract_whenGeneratingAPayload_thenTheExampleIsProperlyGenerated() throws Exception {
-        OpenAPIParser openAPIV3Parser = new OpenAPIParser();
-        ParseOptions options = new ParseOptions();
-        options.setResolve(true);
-        options.setFlatten(true);
-        OpenAPI openAPI = openAPIV3Parser.readContents(new String(Files.readAllBytes(Paths.get("src/test/resources/petstore.yml"))), null, options).getOpenAPI();
-        Map<String, Schema> schemas = CatsMain.getSchemas(openAPI);
-
-        PayloadGenerator generator = new PayloadGenerator(schemas);
+        PayloadGenerator generator = setupPayloadGenerator();
 
         List<Map<String, String>> example = generator.generate(null, "Pet");
         String exampleJson = example.get(0).get("example");
@@ -34,5 +28,38 @@ class PayloadGeneratorTest {
                 .contains("ALL_OF")
                 .contains("breedONE_OF#/components/schemas/Husky")
                 .contains("breedONE_OF#/components/schemas/Labrador");
+    }
+
+    @Test
+    void shouldGenerateOneOfWhenOneOfInRoot() throws Exception {
+        PayloadGenerator generator = setupPayloadGenerator();
+
+        List<Map<String, String>> example = generator.generate(null, "PetType");
+        String exampleJson = example.get(0).get("example");
+
+        Assertions.assertThat(exampleJson)
+                .contains("PetTypeONE_OF#/components/schemas/Husky")
+                .contains("PetTypeONE_OF#/components/schemas/Labrador");
+    }
+
+    @Test
+    void shouldGenerateFromAllOfAndPreserveRootElement() throws Exception {
+        PayloadGenerator generator = setupPayloadGenerator();
+        List<Map<String, String>> example = generator.generate(null, "MiniPet");
+        String exampleJson = example.get(0).get("example");
+
+        Assertions.assertThat(exampleJson).contains("color").contains("red").contains("green").contains("blue");
+    }
+
+    private PayloadGenerator setupPayloadGenerator() throws IOException {
+        OpenAPIParser openAPIV3Parser = new OpenAPIParser();
+        ParseOptions options = new ParseOptions();
+        options.setResolve(true);
+        options.setFlatten(true);
+        OpenAPI openAPI = openAPIV3Parser.readContents(new String(Files.readAllBytes(Paths.get("src/test/resources/petstore.yml"))), null, options).getOpenAPI();
+        Map<String, Schema> schemas = CatsMain.getSchemas(openAPI);
+
+        PayloadGenerator generator = new PayloadGenerator(schemas);
+        return generator;
     }
 }
