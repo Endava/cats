fe955a6dba717dc53585280f0bd51d4d255ed516
Madalin Ilie <madalin.ilie@endava.com>
2021-06-07 15:00:29 +0300

    Run Invisible Chars Fuzzerss also for GET and DELETE

--- src/main/java/com/endava/cats/fuzzer/fields/base/InvisibleCharsBaseTrimValidateFuzzer.java
+++ src/main/java/com/endava/cats/fuzzer/fields/base/InvisibleCharsBaseTrimValidateFuzzer.java
@@ -2,14 +2,12 @@ package com.endava.cats.fuzzer.fields.base;
 
 import com.endava.cats.args.FilesArguments;
 import com.endava.cats.generator.simple.PayloadGenerator;
-import com.endava.cats.http.HttpMethod;
 import com.endava.cats.io.ServiceCaller;
 import com.endava.cats.model.FuzzingData;
 import com.endava.cats.model.FuzzingStrategy;
 import com.endava.cats.report.TestCaseListener;
 import com.endava.cats.util.CatsUtil;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -36,10 +34,6 @@ public abstract class InvisibleCharsBaseTrimValidateFuzzer extends ExpectOnly2XX
         return "iterate through each field and send " + this.typeOfDataSentToTheService();
     }
 
-    @Override
-    public List<HttpMethod> skipFor() {
-        return Arrays.asList(HttpMethod.GET, HttpMethod.DELETE);
-    }
 
     public abstract List<String> getInvisibleChars();
 
