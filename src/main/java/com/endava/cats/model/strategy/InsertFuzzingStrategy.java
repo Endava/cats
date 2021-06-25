3570913005749a0d56a1ee4e1f9f3e879e147de5
Madalin Ilie <madalin.ilie@endava.com>
2021-06-25 21:06:47 +0300

    Add a new FuzzingStrategy that inserts data in the middle of valid values

---
+++ src/main/java/com/endava/cats/model/strategy/InsertFuzzingStrategy.java
@@ -0,0 +1,16 @@
+package com.endava.cats.model.strategy;
+
+import com.endava.cats.fuzzer.fields.within.CommonWithinMethods;
+import com.endava.cats.model.FuzzingStrategy;
+
+public class InsertFuzzingStrategy extends FuzzingStrategy {
+    @Override
+    public String process(String value) {
+        return CommonWithinMethods.insertInTheMiddle(value, data, true);
+    }
+
+    @Override
+    public String name() {
+        return "INSERT";
+    }
+}
