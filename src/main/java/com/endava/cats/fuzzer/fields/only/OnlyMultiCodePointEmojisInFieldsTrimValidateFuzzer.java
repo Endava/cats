7aa8dee1e4db668c7856eb764c5f7732f65a9533
Madalin Ilie <madalin.ilie@endava.com>
2021-07-30 21:21:11 +0300

    Add new Fuzzer for only multi  code point emojis

---
+++ src/main/java/com/endava/cats/fuzzer/fields/only/OnlyMultiCodePointEmojisInFieldsTrimValidateFuzzer.java
@@ -0,0 +1,34 @@
+package com.endava.cats.fuzzer.fields.only;
+
+import com.endava.cats.args.FilesArguments;
+import com.endava.cats.args.FilterArguments;
+import com.endava.cats.fuzzer.FieldFuzzer;
+import com.endava.cats.io.ServiceCaller;
+import com.endava.cats.report.TestCaseListener;
+import com.endava.cats.util.CatsUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@FieldFuzzer
+@ConditionalOnExpression(value = "'${edgeSpacesStrategy:trimAndValidate}'=='trimAndValidate' and ${fuzzer.fields.OnlyMultiCodePointEmojisInFieldsFuzzer.enabled}")
+public class OnlyMultiCodePointEmojisInFieldsTrimValidateFuzzer extends InvisibleCharsOnlyTrimValidateFuzzer {
+
+    @Autowired
+    public OnlyMultiCodePointEmojisInFieldsTrimValidateFuzzer(ServiceCaller sc, TestCaseListener lr, CatsUtil cu, FilesArguments cp, FilterArguments fa) {
+        super(sc, lr, cu, cp, fa);
+    }
+
+    @Override
+    protected String typeOfDataSentToTheService() {
+        return "values with multi code point emojis only";
+    }
+
+    @Override
+    List<String> getInvisibleChars() {
+        return catsUtil.getMultiCodePointEmojis();
+    }
+}
