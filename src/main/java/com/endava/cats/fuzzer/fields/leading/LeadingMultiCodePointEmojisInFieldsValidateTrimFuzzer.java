e1bef72ca59aae26218fa2f9e1519d6e58231376
Madalin Ilie <madalin.ilie@endava.com>
2021-07-30 21:20:12 +0300

    Add new Fuzzer for leading multi code point emojis

---
+++ src/main/java/com/endava/cats/fuzzer/fields/leading/LeadingMultiCodePointEmojisInFieldsValidateTrimFuzzer.java
@@ -0,0 +1,37 @@
+package com.endava.cats.fuzzer.fields.leading;
+
+import com.endava.cats.args.FilesArguments;
+import com.endava.cats.fuzzer.FieldFuzzer;
+import com.endava.cats.fuzzer.http.ResponseCodeFamily;
+import com.endava.cats.io.ServiceCaller;
+import com.endava.cats.report.TestCaseListener;
+import com.endava.cats.util.CatsUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.stereotype.Component;
+
+@Component
+@FieldFuzzer
+@ConditionalOnExpression(value = "'${edgeSpacesStrategy:trimAndValidate}'=='validateAndTrim' and ${fuzzer.fields.LeadingMultiCodePointEmojisInFields.enabled}")
+public class LeadingMultiCodePointEmojisInFieldsValidateTrimFuzzer extends LeadingMultiCodePointEmojisInFieldsTrimValidateFuzzer {
+
+    @Autowired
+    protected LeadingMultiCodePointEmojisInFieldsValidateTrimFuzzer(ServiceCaller sc, TestCaseListener lr, CatsUtil cu, FilesArguments cp) {
+        super(sc, lr, cu, cp);
+    }
+
+    @Override
+    public ResponseCodeFamily getExpectedHttpCodeWhenRequiredFieldsAreFuzzed() {
+        return ResponseCodeFamily.FOURXX;
+    }
+
+    @Override
+    public ResponseCodeFamily getExpectedHttpCodeWhenOptionalFieldsAreFuzzed() {
+        return ResponseCodeFamily.FOURXX;
+    }
+
+    @Override
+    public ResponseCodeFamily getExpectedHttpCodeWhenFuzzedValueNotMatchesPattern() {
+        return ResponseCodeFamily.FOURXX;
+    }
+}
