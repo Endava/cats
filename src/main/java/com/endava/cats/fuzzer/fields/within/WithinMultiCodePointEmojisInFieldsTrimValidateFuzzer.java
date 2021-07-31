3b770402c6647e886fff4e201689207e3bc11a06
Madalin Ilie <madalin.ilie@endava.com>
2021-07-31 21:46:37 +0300

    Add new Fuzzer for inseting multi code point emojis

---
+++ src/main/java/com/endava/cats/fuzzer/fields/within/WithinMultiCodePointEmojisInFieldsTrimValidateFuzzer.java
@@ -0,0 +1,46 @@
+package com.endava.cats.fuzzer.fields.within;
+
+import com.endava.cats.args.FilesArguments;
+import com.endava.cats.fuzzer.FieldFuzzer;
+import com.endava.cats.fuzzer.fields.base.InvisibleCharsBaseTrimValidateFuzzer;
+import com.endava.cats.io.ServiceCaller;
+import com.endava.cats.model.FuzzingData;
+import com.endava.cats.model.FuzzingStrategy;
+import com.endava.cats.report.TestCaseListener;
+import com.endava.cats.util.CatsUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@FieldFuzzer
+@ConditionalOnExpression(value = "'${sanitizationStrategy:sanitizeAndValidate}' == 'sanitizeAndValidate' and ${fuzzer.fields.WithinMultiCodePointEmojisInFieldsFuzzer.enabled}")
+public class WithinMultiCodePointEmojisInFieldsTrimValidateFuzzer extends InvisibleCharsBaseTrimValidateFuzzer {
+
+    @Autowired
+    protected WithinMultiCodePointEmojisInFieldsTrimValidateFuzzer(ServiceCaller sc, TestCaseListener lr, CatsUtil cu, FilesArguments cp) {
+        super(sc, lr, cu, cp);
+    }
+
+    @Override
+    public List<FuzzingStrategy> getFieldFuzzingStrategy(FuzzingData data, String fuzzedField) {
+        return CommonWithinMethods.getFuzzingStrategies(data, fuzzedField, this.getInvisibleChars(), false);
+    }
+
+    @Override
+    protected String typeOfDataSentToTheService() {
+        return "values containing multi code point emojis";
+    }
+
+    @Override
+    public List<String> getInvisibleChars() {
+        return catsUtil.getMultiCodePointEmojis();
+    }
+
+    @Override
+    public FuzzingStrategy concreteFuzzStrategy() {
+        return FuzzingStrategy.replace();
+    }
+}
---
+++ src/test/java/com/endava/cats/fuzzer/fields/within/WithinMultiCodePointEmojisInFieldsTrimValidateFuzzerTest.java
@@ -0,0 +1,66 @@
+package com.endava.cats.fuzzer.fields.within;
+
+import com.endava.cats.args.FilesArguments;
+import com.endava.cats.fuzzer.http.ResponseCodeFamily;
+import com.endava.cats.generator.simple.PayloadGenerator;
+import com.endava.cats.io.ServiceCaller;
+import com.endava.cats.model.FuzzingData;
+import com.endava.cats.model.FuzzingStrategy;
+import com.endava.cats.report.TestCaseListener;
+import com.endava.cats.util.CatsUtil;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.media.StringSchema;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class WithinMultiCodePointEmojisInFieldsTrimValidateFuzzerTest {
+    private final CatsUtil catsUtil = new CatsUtil(null);
+    @Mock
+    private ServiceCaller serviceCaller;
+    @Mock
+    private TestCaseListener testCaseListener;
+    @Mock
+    private FilesArguments filesArguments;
+
+    private WithinMultiCodePointEmojisInFieldsTrimValidateFuzzer withinMultiCodePointEmojisInFieldsTrimValidateFuzzer;
+
+    @BeforeEach
+    void setup() {
+        withinMultiCodePointEmojisInFieldsTrimValidateFuzzer = new WithinMultiCodePointEmojisInFieldsTrimValidateFuzzer(serviceCaller, testCaseListener, catsUtil, filesArguments);
+    }
+
+    @Test
+    void shouldProperlyOverrideSuperClassMethods() {
+        FuzzingData data = Mockito.mock(FuzzingData.class);
+        Map<String, Schema> reqTypes = new HashMap<>();
+        reqTypes.put("field", new StringSchema());
+        Mockito.when(data.getRequestPropertyTypes()).thenReturn(reqTypes);
+        FuzzingStrategy fuzzingStrategy = withinMultiCodePointEmojisInFieldsTrimValidateFuzzer.getFieldFuzzingStrategy(data, "field").get(1);
+        Assertions.assertThat(fuzzingStrategy.name()).isEqualTo(FuzzingStrategy.replace().name());
+
+        Assertions.assertThat(fuzzingStrategy.getData()).contains("\uD83D\uDC68\u200D\uD83C\uDFED️");
+        Assertions.assertThat(withinMultiCodePointEmojisInFieldsTrimValidateFuzzer.getExpectedHttpCodeWhenFuzzedValueNotMatchesPattern()).isEqualTo(ResponseCodeFamily.TWOXX);
+        Assertions.assertThat(withinMultiCodePointEmojisInFieldsTrimValidateFuzzer.description()).isNotNull();
+        Assertions.assertThat(withinMultiCodePointEmojisInFieldsTrimValidateFuzzer.concreteFuzzStrategy().name()).isEqualTo(FuzzingStrategy.replace().name());
+
+        Assertions.assertThat(withinMultiCodePointEmojisInFieldsTrimValidateFuzzer.typeOfDataSentToTheService()).isNotNull();
+    }
+
+    @Test
+    void shouldNotFuzzIfDiscriminatorField() {
+        PayloadGenerator.GlobalData.getDiscriminators().add("pet#type");
+        Assertions.assertThat(withinMultiCodePointEmojisInFieldsTrimValidateFuzzer.isFuzzingPossibleSpecificToFuzzer(null, "pet#type", null)).isFalse();
+    }
+
+    @Test
+    void shouldFuzzIfNotDiscriminatorField() {
+        PayloadGenerator.GlobalData.getDiscriminators().add("pet#type");
+        Assertions.assertThat(withinMultiCodePointEmojisInFieldsTrimValidateFuzzer.isFuzzingPossibleSpecificToFuzzer(null, "pet#number", null)).isTrue();
+    }
+}
