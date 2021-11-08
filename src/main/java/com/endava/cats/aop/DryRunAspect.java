c5b0c7b4c9a6ba33e2ceb5266b1ad03432056d40
Madalin Ilie <madalin.ilie@endava.com>
2021-11-08 16:10:58 +0200

    Make DryRunAspect only available if the dryRun=true

--- src/main/java/com/endava/cats/aop/DryRunAspect.java
+++ src/main/java/com/endava/cats/aop/DryRunAspect.java
@@ -1,7 +1,6 @@
 package com.endava.cats.aop;
 
 import ch.qos.logback.classic.Level;
-import com.endava.cats.args.FilterArguments;
 import com.endava.cats.model.CatsResponse;
 import com.endava.cats.model.FuzzingData;
 import io.github.ludovicianul.prettylogger.PrettyLogger;
@@ -10,11 +9,11 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 import static org.fusesource.jansi.Ansi.ansi;
 
@@ -24,13 +23,11 @@ import static org.fusesource.jansi.Ansi.ansi;
  */
 @Aspect
 @Component
+@ConditionalOnProperty(name = "dryRun", havingValue = "true")
 public class DryRunAspect {
 
     private static final PrettyLogger LOGGER = PrettyLoggerFactory.getLogger(DryRunAspect.class);
-    private final Map<String, Integer> paths = new HashMap<>();
-    private int counter;
-    @Autowired
-    private FilterArguments filterArguments;
+    private final Map<String, Integer> paths = new TreeMap<>();
 
     /**
      * We suppress logging when doing a dry run.
@@ -41,49 +38,38 @@ public class DryRunAspect {
      */
     @Around("execution(* com.endava.cats.args.ReportingArguments.getReportingLevel())")
     public Object reportingLevel(ProceedingJoinPoint joinPoint) throws Throwable {
-        if (filterArguments.isDryRun()) {
-            return "INFO";
-        }
-        return joinPoint.proceed();
+        return "OFF";
     }
 
     @Around("execution(* com.endava.cats.io.ServiceCaller.call(..))")
     public Object dryRunDontInvokeService(ProceedingJoinPoint joinPoint) throws Throwable {
-        if (filterArguments.isDryRun()) {
-            return CatsResponse.empty();
-        }
-        return joinPoint.proceed();
+        return CatsResponse.empty();
     }
 
     @Around("execution(* com.endava.cats.io.TestCaseExporter.writeTestCase(..))")
     public Object dryRunDontWriteTestCase(ProceedingJoinPoint joinPoint) throws Throwable {
-        if (filterArguments.isDryRun()) {
-            return null;
-        }
-        return joinPoint.proceed();
+        return null;
     }
 
     @Around("execution(* com.endava.cats.report.TestCaseListener.endSession(..))")
     public Object endSession(ProceedingJoinPoint joinPoint) throws Throwable {
-        if (filterArguments.isDryRun()) {
-            ((ch.qos.logback.classic.Logger) LoggerFactory.getLogger("com.endava.cats")).setLevel(Level.INFO);
-            LOGGER.note("Number of tests that will be run with this configuration: {}", paths.values().stream().reduce(0, Integer::sum));
-            paths.forEach((s, integer) -> {
-                LOGGER.star(ansi().fgBrightYellow().bold().a(" -> path {}: {} tests").toString(), s, integer);
-            });
-            return null;
-        }
-        return joinPoint.proceed();
+        ((ch.qos.logback.classic.Logger) LoggerFactory.getLogger("com.endava.cats")).setLevel(Level.INFO);
+        LOGGER.note("Number of tests that will be run with this configuration: {}", paths.values().stream().reduce(0, Integer::sum));
+        paths.forEach((s, integer) -> {
+            LOGGER.star(ansi().fgBrightYellow().bold().a(" -> path {}: {} tests").toString(), s, integer);
+        });
+        return null;
+
     }
 
     @Around("execution(* com.endava.cats.report.TestCaseListener.reportInfo(..)) || execution(* com.endava.cats.report.TestCaseListener.reportResult(..))")
     public Object reportError(ProceedingJoinPoint joinPoint) throws Throwable {
-        if (filterArguments.isDryRun()) {
-            FuzzingData data = (FuzzingData) joinPoint.getArgs()[1];
-            paths.merge(data.getPath(), 1, Integer::sum);
-            return null;
+        Object data = joinPoint.getArgs()[1];
+        if (data instanceof FuzzingData) {
+            paths.merge(((FuzzingData) data).getPath(), 1, Integer::sum);
+        } else {
+            paths.merge("contract-level", 1, Integer::sum);
         }
-        return joinPoint.proceed();
+        return null;
     }
-
 }
