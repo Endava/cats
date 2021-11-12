05926976485125afed6a667eb2b17d9dd680c0b1
Madalin Ilie <madalin.ilie@endava.com>
2021-11-12 09:09:59 +0200

    Remove Throwable as not needed and allow dryRun to be interpreted with empty value

--- src/main/java/com/endava/cats/aop/DryRunAspect.java
+++ src/main/java/com/endava/cats/aop/DryRunAspect.java
@@ -23,53 +23,55 @@ import static org.fusesource.jansi.Ansi.ansi;
  */
 @Aspect
 @Component
-@ConditionalOnProperty(name = "dryRun", havingValue = "true")
+@ConditionalOnProperty(name = "dryRun", havingValue = "")
 public class DryRunAspect {
 
     private static final PrettyLogger LOGGER = PrettyLoggerFactory.getLogger(DryRunAspect.class);
     private final Map<String, Integer> paths = new TreeMap<>();
+    private int counter;
 
     /**
      * We suppress logging when doing a dry run.
      *
      * @param joinPoint the AspectJ join point
      * @return "OFF" of the actual log level, depending on the --dryRun arg
-     * @throws Throwable if something happens
      */
     @Around("execution(* com.endava.cats.args.ReportingArguments.getReportingLevel())")
-    public Object reportingLevel(ProceedingJoinPoint joinPoint) throws Throwable {
+    public Object reportingLevel(ProceedingJoinPoint joinPoint) {
         return "OFF";
     }
 
     @Around("execution(* com.endava.cats.io.ServiceCaller.call(..))")
-    public Object dryRunDontInvokeService(ProceedingJoinPoint joinPoint) throws Throwable {
+    public Object dryRunDontInvokeService(ProceedingJoinPoint joinPoint) {
         return CatsResponse.empty();
     }
 
     @Around("execution(* com.endava.cats.io.TestCaseExporter.writeTestCase(..))")
-    public Object dryRunDontWriteTestCase(ProceedingJoinPoint joinPoint) throws Throwable {
+    public Object dryRunDontWriteTestCase(ProceedingJoinPoint joinPoint) {
         return null;
     }
 
     @Around("execution(* com.endava.cats.report.TestCaseListener.endSession(..))")
-    public Object endSession(ProceedingJoinPoint joinPoint) throws Throwable {
+    public Object endSession(ProceedingJoinPoint joinPoint) {
+        System.out.print("\n");
         ((ch.qos.logback.classic.Logger) LoggerFactory.getLogger("com.endava.cats")).setLevel(Level.INFO);
         LOGGER.note("Number of tests that will be run with this configuration: {}", paths.values().stream().reduce(0, Integer::sum));
-        paths.forEach((s, integer) -> {
-            LOGGER.star(ansi().fgBrightYellow().bold().a(" -> path {}: {} tests").toString(), s, integer);
-        });
+        paths.forEach((s, integer) -> LOGGER.star(ansi().fgBrightYellow().bold().a(" -> path {}: {} tests").toString(), s, integer));
         return null;
-
     }
 
     @Around("execution(* com.endava.cats.report.TestCaseListener.reportInfo(..)) || execution(* com.endava.cats.report.TestCaseListener.reportResult(..))")
-    public Object reportError(ProceedingJoinPoint joinPoint) throws Throwable {
+    public Object report(ProceedingJoinPoint joinPoint) {
         Object data = joinPoint.getArgs()[1];
         if (data instanceof FuzzingData) {
+            if (counter % 10000 == 0) {
+                System.out.print(".");
+            }
             paths.merge(((FuzzingData) data).getPath(), 1, Integer::sum);
         } else {
             paths.merge("contract-level", 1, Integer::sum);
         }
+        counter++;
         return null;
     }
 }
