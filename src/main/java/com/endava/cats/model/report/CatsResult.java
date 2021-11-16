ba789fe45bd03fa302659ee192a05cb2665248f8
Madalin Ilie <madalin.ilie@endava.com>
2021-11-16 17:34:31 +0200

    Include 404 as a special result reason category

--- src/main/java/com/endava/cats/model/report/CatsResult.java
+++ src/main/java/com/endava/cats/model/report/CatsResult.java
@@ -8,6 +8,7 @@ public enum CatsResult {
     UNDOCUMENTED_RESPONSE_CODE("Response does NOT match expected result. Response code is from a list of expected codes for this FUZZER, but it is undocumented: expected {}, actual [{}], documented response codes: {}", "Undocumented Response Code"),
     UNEXPECTED_RESPONSE_CODE("Response does NOT match expected result. Response code is NOT from a list of expected codes for this FUZZER: expected {}, actual [{}]", "Unexpected Response Code"),
     NOT_IMPLEMENTED("Response HTTP code 501: you forgot to implement this functionality!", "Not Implemented"),
+    NOT_FOUND("Response HTTP code 404: you might need to provide business context using --refData or --urlParams", "Not Found"),
     UNEXPECTED_BEHAVIOUR("Unexpected behaviour: expected {}, actual [{}]", "Unexpected behaviour"),
     EXCEPTION("Fuzzer [{}] failed due to [{}]", "Unexpected Exception");
 
