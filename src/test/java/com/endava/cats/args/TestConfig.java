4ac899f73d865d72fafd25fc19cae8acc1bced31
Madalin Ilie <madalin.ilie@endava.com>
2021-12-09 23:05:21 +0200

    Update libraries to latest versions

--- src/test/java/com/endava/cats/args/TestConfig.java
+++
@@ -1,9 +0,0 @@
-package com.endava.cats.args;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@ComponentScan(basePackages = {"com.endava.cats"})
-@Configuration
-public class TestConfig {
-}
