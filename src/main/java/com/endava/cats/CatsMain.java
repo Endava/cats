da5d05e0598e0a188d57cedf7c190f13337d093a
Madalin Ilie <madalin.ilie@endava.com>
2021-12-17 22:51:45 +0200

    Tweak color scheme when displaying help

--- src/main/java/com/endava/cats/CatsMain.java
+++ src/main/java/com/endava/cats/CatsMain.java
@@ -23,6 +23,18 @@ public class CatsMain implements QuarkusApplication {
     @Override
     public int run(String... args) {
         return new CommandLine(catsCommand, factory)
-                .setCaseInsensitiveEnumValuesAllowed(true).execute(args);
+                .setCaseInsensitiveEnumValuesAllowed(true).setColorScheme(colorScheme())
+                .execute(args);
+    }
+
+    CommandLine.Help.ColorScheme colorScheme() {
+        return new CommandLine.Help.ColorScheme.Builder()
+                .commands(CommandLine.Help.Ansi.Style.bold)
+                .options(CommandLine.Help.Ansi.Style.fg_yellow)
+                .parameters(CommandLine.Help.Ansi.Style.fg_yellow)
+                .optionParams(CommandLine.Help.Ansi.Style.faint)
+                .errors(CommandLine.Help.Ansi.Style.fg_red, CommandLine.Help.Ansi.Style.bold)
+                .stackTraces(CommandLine.Help.Ansi.Style.italic)
+                .build();
     }
 }
