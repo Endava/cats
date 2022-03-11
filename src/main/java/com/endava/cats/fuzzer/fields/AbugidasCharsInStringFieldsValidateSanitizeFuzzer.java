31ee2d3c8fa0cf228d86d19b4e949f13e62be147
Madalin Ilie <madalin.ilie@endava.com>
2022-03-11 20:33:25 +0200

    Reduce code duplication by changing base class

--- src/main/java/com/endava/cats/fuzzer/fields/AbugidasCharsInStringFieldsValidateSanitizeFuzzer.java
+++ src/main/java/com/endava/cats/fuzzer/fields/AbugidasCharsInStringFieldsValidateSanitizeFuzzer.java
@@ -3,26 +3,17 @@ package com.endava.cats.fuzzer.fields;
 import com.endava.cats.annotations.FieldFuzzer;
 import com.endava.cats.annotations.ValidateAndSanitize;
 import com.endava.cats.args.FilesArguments;
-import com.endava.cats.fuzzer.fields.base.InvisibleCharsBaseTrimValidateFuzzer;
 import com.endava.cats.http.ResponseCodeFamily;
 import com.endava.cats.io.ServiceCaller;
-import com.endava.cats.model.CommonWithinMethods;
-import com.endava.cats.model.FuzzingData;
-import com.endava.cats.model.FuzzingStrategy;
-import com.endava.cats.model.util.PayloadUtils;
 import com.endava.cats.report.TestCaseListener;
 import com.endava.cats.util.CatsUtil;
-import io.swagger.v3.oas.models.media.Schema;
 
 import javax.inject.Singleton;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Singleton
 @FieldFuzzer
 @ValidateAndSanitize
-public class AbugidasCharsInStringFieldsValidateSanitizeFuzzer extends InvisibleCharsBaseTrimValidateFuzzer {
+public class AbugidasCharsInStringFieldsValidateSanitizeFuzzer extends AbugidasCharsInStringFieldsSanitizeValidateFuzzer {
     protected AbugidasCharsInStringFieldsValidateSanitizeFuzzer(ServiceCaller sc, TestCaseListener lr, CatsUtil cu, FilesArguments cp) {
         super(sc, lr, cu, cp);
     }
@@ -41,28 +32,5 @@ public class AbugidasCharsInStringFieldsValidateSanitizeFuzzer extends Invisible
     public ResponseCodeFamily getExpectedHttpCodeWhenOptionalFieldsAreFuzzed() {
         return ResponseCodeFamily.FOURXX;
     }
-
-    @Override
-    public List<FuzzingStrategy> getFieldFuzzingStrategy(FuzzingData data, String fuzzedField) {
-        Schema<?> fuzzedFieldSchema = data.getRequestPropertyTypes().get(fuzzedField);
-        return PayloadUtils.getAbugidasChars()
-                .stream()
-                .map(abugidasChar -> CommonWithinMethods.getTextBasedOnMaxSize(fuzzedFieldSchema, abugidasChar))
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    protected String typeOfDataSentToTheService() {
-        return "values containing abugidas chars";
-    }
-
-    @Override
-    public List<String> getInvisibleChars() {
-        return Collections.emptyList();
-    }
-
-    @Override
-    public FuzzingStrategy concreteFuzzStrategy() {
-        return FuzzingStrategy.replace();
-    }
+    
 }
