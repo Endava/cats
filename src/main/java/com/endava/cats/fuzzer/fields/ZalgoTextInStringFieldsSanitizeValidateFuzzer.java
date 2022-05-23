fdc2b8e87de7547521a260fbd2a93a40dda310c5
Madalin Ilie <madalin.ilie@endava.com>
2022-05-23 22:21:22 +0300

    Make Zalgo text prefix valid values
    The previous implementation was assuming a fixed text with zalgo in it
    whihch is not actually correct as you need valid values to pass validation

--- src/main/java/com/endava/cats/fuzzer/fields/ZalgoTextInStringFieldsSanitizeValidateFuzzer.java
+++ src/main/java/com/endava/cats/fuzzer/fields/ZalgoTextInStringFieldsSanitizeValidateFuzzer.java
@@ -5,13 +5,13 @@ import com.endava.cats.annotations.SanitizeAndValidate;
 import com.endava.cats.args.FilesArguments;
 import com.endava.cats.fuzzer.fields.base.InvisibleCharsBaseTrimValidateFuzzer;
 import com.endava.cats.io.ServiceCaller;
-import com.endava.cats.model.CommonWithinMethods;
 import com.endava.cats.model.FuzzingData;
 import com.endava.cats.model.FuzzingStrategy;
 import com.endava.cats.model.util.PayloadUtils;
 import com.endava.cats.report.TestCaseListener;
 import com.endava.cats.util.CatsUtil;
 import io.swagger.v3.oas.models.media.Schema;
+import org.springframework.util.CollectionUtils;
 
 import javax.inject.Singleton;
 import java.util.Collections;
@@ -44,6 +44,13 @@ public class ZalgoTextInStringFieldsSanitizeValidateFuzzer extends InvisibleChar
     @Override
     public List<FuzzingStrategy> getFieldFuzzingStrategy(FuzzingData data, String fuzzedField) {
         Schema<?> fuzzedFieldSchema = data.getRequestPropertyTypes().get(fuzzedField);
-        return List.of(CommonWithinMethods.getTextBasedOnMaxSize(fuzzedFieldSchema, PayloadUtils.getZalgoText()));
+        return List.of(FuzzingStrategy.prefix().withData(PayloadUtils.getZalgoText()));
+    }
+
+    @Override
+    public boolean isFuzzingPossibleSpecificToFuzzer(FuzzingData data, String fuzzedField, FuzzingStrategy fuzzingStrategy) {
+        Schema<?> fuzzedFieldSchema = data.getRequestPropertyTypes().get(fuzzedField);
+
+        return CollectionUtils.isEmpty(fuzzedFieldSchema.getEnum());
     }
 }
