ad522ca856ad7ab2554fd3eb5c12e67849f84f9e
Madalin Ilie <madalin.ilie@endava.com>
2022-09-08 23:21:39 +0300

    Add timestamp of test case in final report

--- src/main/java/com/endava/cats/model/CatsRequest.java
+++ src/main/java/com/endava/cats/model/CatsRequest.java
@@ -1,10 +1,11 @@
 package com.endava.cats.model;
 
-import com.endava.cats.model.KeyValuePair;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.List;
 
@@ -17,6 +18,9 @@ public class CatsRequest {
     String httpMethod;
     String url;
 
+    @Builder.Default
+    String timestamp = DateTimeFormatter.RFC_1123_DATE_TIME.format(OffsetDateTime.now());
+
     public static CatsRequest empty() {
         CatsRequest request = CatsRequest.builder().build();
         request.payload = "{}";
@@ -25,4 +29,5 @@ public class CatsRequest {
         request.headers = Collections.emptyList();
         return request;
     }
+
 }
