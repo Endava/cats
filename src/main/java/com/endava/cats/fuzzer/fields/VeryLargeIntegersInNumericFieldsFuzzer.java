79c0b3c63eaa871eacbbda2f5abc7662cbf9446e
Madalin Ilie <madalin.ilie@endava.com>
2022-09-12 20:10:46 +0300

    Add new fuzzer with large integers in numeric fields

---
+++ src/main/java/com/endava/cats/fuzzer/fields/VeryLargeIntegersInNumericFieldsFuzzer.java
@@ -0,0 +1,55 @@
+package com.endava.cats.fuzzer.fields;
+
+import com.endava.cats.annotations.FieldFuzzer;
+import com.endava.cats.args.FilesArguments;
+import com.endava.cats.args.ProcessingArguments;
+import com.endava.cats.fuzzer.fields.base.ExpectOnly4XXBaseFieldsFuzzer;
+import com.endava.cats.generator.simple.NumberGenerator;
+import com.endava.cats.io.ServiceCaller;
+import com.endava.cats.model.FuzzingData;
+import com.endava.cats.model.FuzzingStrategy;
+import com.endava.cats.report.TestCaseListener;
+import com.endava.cats.util.CatsUtil;
+import io.swagger.v3.oas.models.media.IntegerSchema;
+import io.swagger.v3.oas.models.media.NumberSchema;
+import io.swagger.v3.oas.models.media.Schema;
+
+import javax.inject.Singleton;
+import java.util.Collections;
+import java.util.List;
+
+@Singleton
+@FieldFuzzer
+public class VeryLargeIntegersInNumericFieldsFuzzer extends ExpectOnly4XXBaseFieldsFuzzer {
+
+    final ProcessingArguments processingArguments;
+
+    public VeryLargeIntegersInNumericFieldsFuzzer(ServiceCaller sc, TestCaseListener lr, CatsUtil cu, FilesArguments cp, ProcessingArguments pa) {
+        super(sc, lr, cu, cp);
+        this.processingArguments = pa;
+    }
+
+    @Override
+    public String typeOfDataSentToTheService() {
+        return "very large numbers";
+    }
+
+    @Override
+    protected List<FuzzingStrategy> getFieldFuzzingStrategy(FuzzingData data, String fuzzedField) {
+        Schema<?> fieldSchema = data.getRequestPropertyTypes().get(fuzzedField);
+        if (fieldSchema instanceof NumberSchema || fieldSchema instanceof IntegerSchema) {
+            return Collections.singletonList(FuzzingStrategy.replace().withData(this.getTheActualData()));
+        } else {
+            return List.of(FuzzingStrategy.skip().withData("field is not numeric"));
+        }
+    }
+
+    public String getTheActualData() {
+        return NumberGenerator.generateVeryLargeInteger(processingArguments.getLargeStringsSize() / 4);
+    }
+
+    @Override
+    public String description() {
+        return "iterate through each numeric field and send requests with very large numbers (40000 characters) in the targeted field";
+    }
+}
---
+++ src/test/java/com/endava/cats/fuzzer/fields/VeryLargeIntegersInNumericFieldsFuzzerTest.java
@@ -0,0 +1,60 @@
+package com.endava.cats.fuzzer.fields;
+
+import com.endava.cats.args.ProcessingArguments;
+import com.endava.cats.model.FuzzingData;
+import io.quarkus.test.junit.QuarkusTest;
+import io.swagger.v3.oas.models.media.IntegerSchema;
+import io.swagger.v3.oas.models.media.NumberSchema;
+import io.swagger.v3.oas.models.media.StringSchema;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Map;
+
+@QuarkusTest
+class VeryLargeIntegersInNumericFieldsFuzzerTest {
+    private VeryLargeIntegersInNumericFieldsFuzzer veryLargeIntegersInNumericFieldsFuzzer;
+    private ProcessingArguments processingArguments;
+
+    @BeforeEach
+    void setup() {
+        processingArguments = Mockito.mock(ProcessingArguments.class);
+        veryLargeIntegersInNumericFieldsFuzzer = new VeryLargeIntegersInNumericFieldsFuzzer(null, null, null, null, processingArguments);
+    }
+
+    @Test
+    void shouldReturnDescriptionAndTypeOfData() {
+        Assertions.assertThat(veryLargeIntegersInNumericFieldsFuzzer.description()).isNotNull();
+        Assertions.assertThat(veryLargeIntegersInNumericFieldsFuzzer.typeOfDataSentToTheService()).isNotNull();
+
+    }
+
+    @Test
+    void shouldGetPayloadSizeForNumberSchema() {
+        Mockito.when(processingArguments.getLargeStringsSize()).thenReturn(20000);
+        FuzzingData data = Mockito.mock(FuzzingData.class);
+        Mockito.when(data.getRequestPropertyTypes()).thenReturn(Map.of("myField", new NumberSchema()));
+
+        Assertions.assertThat(veryLargeIntegersInNumericFieldsFuzzer.getFieldFuzzingStrategy(data, "myField").get(0).getData().toString()).hasSize(20000);
+    }
+
+    @Test
+    void shouldGetPayloadSizeForIntegerSchema() {
+        Mockito.when(processingArguments.getLargeStringsSize()).thenReturn(30000);
+        FuzzingData data = Mockito.mock(FuzzingData.class);
+        Mockito.when(data.getRequestPropertyTypes()).thenReturn(Map.of("myField", new IntegerSchema()));
+
+        Assertions.assertThat(veryLargeIntegersInNumericFieldsFuzzer.getFieldFuzzingStrategy(data, "myField").get(0).getData().toString()).hasSize(30000);
+    }
+
+    @Test
+    void shouldSkipWhenNotNumericFields() {
+        Mockito.when(processingArguments.getLargeStringsSize()).thenReturn(30000);
+        FuzzingData data = Mockito.mock(FuzzingData.class);
+        Mockito.when(data.getRequestPropertyTypes()).thenReturn(Map.of("myField", new StringSchema()));
+
+        Assertions.assertThat(veryLargeIntegersInNumericFieldsFuzzer.getFieldFuzzingStrategy(data, "myField").get(0).isSkip()).isTrue();
+    }
+}
