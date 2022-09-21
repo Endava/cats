51e8ef41ab316a53eea49c1d44494fcd97ef69ac
Madalin Ilie <madalin.ilie@endava.com>
2022-09-21 20:07:44 +0300

    Allw cats run to parse simple headers using -H

--- src/main/java/com/endava/cats/command/RunCommand.java
+++ src/main/java/com/endava/cats/command/RunCommand.java
@@ -16,6 +16,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
+import java.util.Map;
 
 /**
  * This will run custom tests written using the CATS YAML format.
@@ -58,6 +59,10 @@ public class RunCommand implements Runnable {
             description = "Specifies custom headers that will be passed along with request. This can be used to pass oauth or JWT tokens for authentication purposed for example")
     private File headersFile;
 
+    @CommandLine.Option(names = {"-H"},
+            description = "Specifies the headers that will be passed along with the request. When supplied it will be applied to ALL paths. For per-path control use the `--headers` arg that requires a file.")
+    Map<String, String> headersMap;
+
     @CommandLine.Option(names = {"--createRefData"},
             description = "This is only applicable when enabling the @|bold FunctionalFuzzer |@. It will instruct the @|bold FunctionalFuzzer|@ to create a @|bold,underline --refData|@ file " +
                     "with all the paths defined in the @|bold,underline customFuzzerFile|@ and the corresponding @|bold output|@ variables")
@@ -91,6 +96,7 @@ public class RunCommand implements Runnable {
                 catsCommand.filesArguments.setSecurityFuzzerFile(file);
             }
             catsCommand.filesArguments.setHeadersFile(headersFile);
+            catsCommand.filesArguments.setHeadersMap(headersMap);
             catsCommand.filesArguments.setCreateRefData(createRefData);
             catsCommand.filesArguments.setRefDataFile(refDataFile);
             catsCommand.processingArguments.setContentType(this.contentType);
