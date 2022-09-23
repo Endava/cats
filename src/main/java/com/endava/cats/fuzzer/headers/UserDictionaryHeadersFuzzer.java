f583843dce3f7bed067fb337aaba867a51ba7b26
Madalin Ilie <madalin.ilie@endava.com>
2022-09-23 20:16:07 +0300

    Add Custom User Dictionary Fuzzer for headers

--- src/main/java/com/endava/cats/fuzzer/headers/UserDictionaryHeadersFuzzer.java
+++ src/main/java/com/endava/cats/fuzzer/headers/UserDictionaryHeadersFuzzer.java
@@ -2,17 +2,76 @@ package com.endava.cats.fuzzer.headers;
 
 import com.endava.cats.Fuzzer;
 import com.endava.cats.annotations.HeaderFuzzer;
+import com.endava.cats.args.MatchArguments;
+import com.endava.cats.args.UserArguments;
+import com.endava.cats.generator.Cloner;
+import com.endava.cats.io.ServiceCaller;
+import com.endava.cats.io.ServiceData;
+import com.endava.cats.model.CatsHeader;
+import com.endava.cats.model.CatsResponse;
 import com.endava.cats.model.FuzzingData;
+import com.endava.cats.report.TestCaseListener;
 import com.endava.cats.util.ConsoleUtils;
+import io.github.ludovicianul.prettylogger.PrettyLogger;
+import io.github.ludovicianul.prettylogger.PrettyLoggerFactory;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.Collection;
 
 @Singleton
 @HeaderFuzzer
 public class UserDictionaryHeadersFuzzer implements Fuzzer {
+    private final PrettyLogger logger = PrettyLoggerFactory.getLogger(UserDictionaryHeadersFuzzer.class);
+    private final TestCaseListener testCaseListener;
+    private final UserArguments userArguments;
+    private final MatchArguments matchArguments;
+    private final ServiceCaller serviceCaller;
+
+    @Inject
+    public UserDictionaryHeadersFuzzer(TestCaseListener testCaseListener, UserArguments userArguments, MatchArguments matchArguments, ServiceCaller sc) {
+        this.testCaseListener = testCaseListener;
+        this.userArguments = userArguments;
+        this.matchArguments = matchArguments;
+        this.serviceCaller = sc;
+    }
+
     @Override
     public void fuzz(FuzzingData data) {
+        if (userArguments.getWords() == null) {
+            logger.error("Skipping fuzzer as --words was not provided!");
+        } else if (!matchArguments.isAnyMatchArgumentSupplied()) {
+            logger.error("Skipping fuzzer as no --m* argument was provided!");
+        } else {
+            Collection<CatsHeader> headers = Cloner.cloneMe(data.getHeaders());
 
+            for (CatsHeader header : headers) {
+                String previousValue = header.getValue();
+
+                try {
+                    for (String fuzzValue : userArguments.getWordsAsList()) {
+                        header.withValue(fuzzValue);
+                        testCaseListener.createAndExecuteTest(logger, this, () -> {
+                            testCaseListener.addScenario(logger, "Iterate through each header and send values from user dictionary. Header [{}] with [{}]", header.getName(), header.getValue());
+                            testCaseListener.addExpectedResult(logger, "Should return a valid response");
+
+                            ServiceData serviceData = ServiceData.builder().relativePath(data.getPath()).headers(headers)
+                                    .payload(data.getPayload()).fuzzedHeader(header.getName()).queryParams(data.getQueryParams()).httpMethod(data.getMethod())
+                                    .contentType(data.getFirstRequestContentType()).build();
+
+                            CatsResponse response = serviceCaller.call(serviceData);
+                            if (matchArguments.isMatchResponse(response)) {
+                                testCaseListener.reportError(logger, "Service call completed. Please check response details");
+                            } else {
+                                testCaseListener.skipTest(logger, "Skipping test as response does not match given matchers!");
+                            }
+                        });
+                    }
+                } finally {
+                    header.withValue(previousValue);
+                }
+            }
+        }
     }
 
     @Override
---
+++ src/test/java/com/endava/cats/fuzzer/headers/UserDictionaryHeadersFuzzerTest.java
@@ -0,0 +1,97 @@
+package com.endava.cats.fuzzer.headers;
+
+import com.endava.cats.args.MatchArguments;
+import com.endava.cats.args.UserArguments;
+import com.endava.cats.io.ServiceCaller;
+import com.endava.cats.model.CatsHeader;
+import com.endava.cats.model.FuzzingData;
+import com.endava.cats.report.TestCaseExporter;
+import com.endava.cats.report.TestCaseListener;
+import io.github.ludovicianul.prettylogger.PrettyLogger;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectSpy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.io.File;
+import java.util.List;
+import java.util.Set;
+
+@QuarkusTest
+class UserDictionaryHeadersFuzzerTest {
+    private UserArguments userArguments;
+    private MatchArguments matchArguments;
+    private UserDictionaryHeadersFuzzer userDictionaryHeadersFuzzer;
+    @InjectSpy
+    private TestCaseListener testCaseListener;
+    private ServiceCaller serviceCaller;
+    private PrettyLogger prettyLogger;
+
+    @BeforeEach
+    void setup() {
+        serviceCaller = Mockito.mock(ServiceCaller.class);
+        userArguments = Mockito.mock(UserArguments.class);
+        matchArguments = Mockito.mock(MatchArguments.class);
+        prettyLogger = Mockito.mock(PrettyLogger.class);
+        ReflectionTestUtils.setField(testCaseListener, "testCaseExporter", Mockito.mock(TestCaseExporter.class));
+
+        userDictionaryHeadersFuzzer = new UserDictionaryHeadersFuzzer(testCaseListener, userArguments, matchArguments, serviceCaller);
+        ReflectionTestUtils.setField(userDictionaryHeadersFuzzer, "logger", prettyLogger);
+    }
+
+    @Test
+    void shouldHaveDescription() {
+        Assertions.assertThat(userDictionaryHeadersFuzzer.description()).isNotBlank();
+    }
+
+    @Test
+    void shouldHaveToString() {
+        Assertions.assertThat(userDictionaryHeadersFuzzer).hasToString(userDictionaryHeadersFuzzer.getClass().getSimpleName());
+    }
+
+    @Test
+    void shouldNotRunWhenWordListIsNull() {
+        userDictionaryHeadersFuzzer.fuzz(Mockito.mock(FuzzingData.class));
+
+        Mockito.verify(prettyLogger, Mockito.times(1)).error("Skipping fuzzer as --words was not provided!");
+        Mockito.verifyNoInteractions(testCaseListener);
+    }
+
+    @Test
+    void shouldNotRunWhenNoMatchArgumentsSupplied() {
+        Mockito.when(userArguments.getWords()).thenReturn(new File("test"));
+        userDictionaryHeadersFuzzer.fuzz(Mockito.mock(FuzzingData.class));
+
+        Mockito.verify(prettyLogger, Mockito.times(1)).error("Skipping fuzzer as no --m* argument was provided!");
+        Mockito.verifyNoInteractions(testCaseListener);
+    }
+
+    @Test
+    void shouldReportErrorWhenResponseMatches() {
+        Mockito.when(userArguments.getWordsAsList()).thenReturn(List.of("headerFuzz"));
+        Mockito.when(userArguments.getWords()).thenReturn(new File("test"));
+        Mockito.when(matchArguments.isMatchResponse(Mockito.any())).thenReturn(true);
+        Mockito.when(matchArguments.isAnyMatchArgumentSupplied()).thenReturn(true);
+        FuzzingData data = Mockito.mock(FuzzingData.class);
+        Mockito.when(data.getHeaders()).thenReturn(Set.of(CatsHeader.builder().name("header1").value("value").build()));
+        userDictionaryHeadersFuzzer.fuzz(data);
+
+        Mockito.verify(testCaseListener, Mockito.times(1)).reportError(Mockito.any(), Mockito.eq("Service call completed. Please check response details"), Mockito.any());
+    }
+
+    @Test
+    void shouldSkipWhenResponseNotMatches() {
+        Mockito.when(userArguments.getWordsAsList()).thenReturn(List.of("headerFuzz"));
+        Mockito.when(userArguments.getWords()).thenReturn(new File("test"));
+        Mockito.when(matchArguments.isMatchResponse(Mockito.any())).thenReturn(false);
+        Mockito.when(matchArguments.isAnyMatchArgumentSupplied()).thenReturn(true);
+        FuzzingData data = Mockito.mock(FuzzingData.class);
+        Mockito.when(data.getHeaders()).thenReturn(Set.of(CatsHeader.builder().name("header1").value("value").build()));
+        userDictionaryHeadersFuzzer.fuzz(data);
+
+        Mockito.verify(testCaseListener, Mockito.times(1)).skipTest(Mockito.any(), Mockito.anyString());
+    }
+}
