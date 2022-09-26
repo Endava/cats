01935a1391c44e186c499e3f151b9c08440eaf19
Madalin Ilie <madalin.ilie@endava.com>
2022-09-26 22:39:22 +0300

    Add documentation about the FieldsIteratorExecutor

--- src/main/java/com/endava/cats/fuzzer/executor/FieldsIteratorExecutor.java
+++ src/main/java/com/endava/cats/fuzzer/executor/FieldsIteratorExecutor.java
@@ -19,15 +19,34 @@ import java.util.Set;
 
 import static com.endava.cats.io.ServiceCaller.CATS_REMOVE_FIELD;
 
+/**
+ * Executors are meant to be the units that will execute the fuzzer's payloads and facilitate the interaction with all the reporting.
+ * <p>
+ * <b>
+ * They are not responsible for the creation of the payloads, but only for making sure the tests are executed within a testing context
+ * and results are reported accordingly.
+ * </b>
+ * </p>
+ * <p>
+ * {@code FieldsIteratorExecutor} is an Executor that iterates through all fields and applies the supplied {@code FieldsIteratorExecutorContext#fuzzingStrategy}.
+ * This is useful when Fuzzers want to fuzz each request field iteratively.
+ * </p>
+ * <p>
+ * By default, the Executor will call {@code TestCaseListener#reportResult} matching against the supplied {@code FieldsIteratorExecutorContext#expectedResponseCode}.
+ * You can also supply {@code FieldsIteratorExecutorContext#schemaFiler} and/or a  {@code FieldsIteratorExecutorContext#fieldFilter}if you want to skip reporting for specific schemas or fields.
+ * If {@code matchArguments are supplied} and no {@code FieldsIteratorExecutorContext#expectedResponseCode} the Executor will match the arguments against the response.
+ * </p>
+ * <p>
+ * You must also supply a  {@code FieldsIteratorExecutorContext#fuzzValueProducer} for the Executor that will be used as the source of fuzzing.
+ * </p>
+ */
 @Singleton
 public class FieldsIteratorExecutor {
 
     private final ServiceCaller serviceCaller;
     private final TestCaseListener testCaseListener;
     private final CatsUtil catsUtil;
-
     private final MatchArguments matchArguments;
-
     private final FilesArguments filesArguments;
 
     @Inject
@@ -62,9 +81,16 @@ public class FieldsIteratorExecutor {
 
                                 FuzzingResult fuzzingResult = catsUtil.replaceField(context.getFuzzingData().getPayload(), fuzzedField, strategy);
 
-                                CatsResponse response = serviceCaller.call(ServiceData.builder().relativePath(context.getFuzzingData().getPath()).headers(context.getFuzzingData().getHeaders())
-                                        .payload(fuzzingResult.getJson()).queryParams(context.getFuzzingData().getQueryParams()).httpMethod(context.getFuzzingData().getMethod())
-                                        .contentType(context.getFuzzingData().getFirstRequestContentType()).build());
+                                CatsResponse response = serviceCaller.call(
+                                        ServiceData.builder()
+                                                .relativePath(context.getFuzzingData().getPath())
+                                                .headers(context.getFuzzingData().getHeaders())
+                                                .payload(fuzzingResult.getJson())
+                                                .queryParams(context.getFuzzingData().getQueryParams())
+                                                .httpMethod(context.getFuzzingData().getMethod())
+                                                .contentType(context.getFuzzingData().getFirstRequestContentType())
+                                                .replaceRefData(context.isReplaceRefData())
+                                                .build());
 
                                 if (context.getExpectedResponseCode() != null) {
                                     testCaseListener.reportResult(context.getLogger(), context.getFuzzingData(), response, context.getExpectedResponseCode());
