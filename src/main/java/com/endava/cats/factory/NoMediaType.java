da20860ada3caf7f414d010ddeda1fb9b5f14fe3
Madalin Ilie <madalin.ilie@endava.com>
2022-10-03 20:12:11 +0300

    Handle case when request is PUT or POST but body is empty

---
+++ src/main/java/com/endava/cats/factory/NoMediaType.java
@@ -0,0 +1,19 @@
+package com.endava.cats.factory;
+
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.media.Schema;
+
+public class NoMediaType extends MediaType {
+    public static final String EMPTY_BODY = "empty_body";
+    public static final Schema<?> EMPTY_BODY_SCHEMA;
+
+    static {
+        EMPTY_BODY_SCHEMA = new Schema<>();
+        EMPTY_BODY_SCHEMA.set$ref(EMPTY_BODY);
+        EMPTY_BODY_SCHEMA.setExample("{}");
+    }
+
+    public Schema<?> getSchema() {
+        return EMPTY_BODY_SCHEMA;
+    }
+}
