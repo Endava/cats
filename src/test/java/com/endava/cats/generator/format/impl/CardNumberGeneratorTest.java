8a6fade991b0b3b7117fce4c00266c80b0882a6f
Madalin Ilie <madalin.ilie@endava.com>
2022-11-23 20:57:11 +0200

    Fix failing unit test

--- src/test/java/com/endava/cats/generator/format/impl/CardNumberGeneratorTest.java
+++ src/test/java/com/endava/cats/generator/format/impl/CardNumberGeneratorTest.java
@@ -14,7 +14,7 @@ class CardNumberGeneratorTest {
     void shouldGenerate() {
         CardNumberGenerator cardNumberGenerator = new CardNumberGenerator();
         String generated = cardNumberGenerator.generate(new Schema<>()).toString();
-        Assertions.assertThat(generated).contains(CardNumberGenerator.CARDS);
+        Assertions.assertThat(generated).matches(CardNumberGenerator.CARDS::contains);
     }
 
     @ParameterizedTest
