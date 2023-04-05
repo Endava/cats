15e580696428926c64debef95ab403e5d18f8c74
Madalin Ilie <madalin.ilie@endava.com>
2023-04-05 20:10:35 +0300

    Enhance the capabilities of expressions that can be used in custom files
    You can now use more complex expression by refering request, response
    or output variables from previous tests

--- src/main/java/com/endava/cats/dsl/CatsDSLParser.java
+++ src/main/java/com/endava/cats/dsl/CatsDSLParser.java
@@ -3,39 +3,45 @@ package com.endava.cats.dsl;
 import com.endava.cats.dsl.api.Parser;
 import com.endava.cats.dsl.impl.AuthScriptProviderParser;
 import com.endava.cats.dsl.impl.EnvVariableParser;
-import com.endava.cats.dsl.impl.RequestVariableParser;
 import com.endava.cats.dsl.impl.SpringELParser;
-import io.github.ludovicianul.prettylogger.PrettyLogger;
-import io.github.ludovicianul.prettylogger.PrettyLoggerFactory;
 
 import javax.inject.Singleton;
 import java.util.Map;
-import java.util.Optional;
 
 @Singleton
 public class CatsDSLParser {
-
-    private final PrettyLogger logger = PrettyLoggerFactory.getLogger(CatsDSLParser.class);
     private static final Map<String, Parser> PARSERS = Map.of(
-            "T(", new SpringELParser(),
             "$$", new EnvVariableParser(),
-            "$request", new RequestVariableParser(),
             "auth_script", new AuthScriptProviderParser());
 
     /**
      * Gets the appropriate parser based on the {@code valueFromFile} and runs it against the given payload.
+     * If no Parser is found, it will default to {@link SpringELParser}.
      *
      * @param valueFromFile the expression retrieved from the CATS files
      * @param context       a given context: JSON request or response, a Map of values
      * @return the result after the appropriate parser runs
      */
     public String parseAndGetResult(String valueFromFile, Map<String, String> context) {
-        Optional<Map.Entry<String, Parser>> parserEntry = PARSERS.entrySet().stream().filter(entry -> valueFromFile.startsWith(entry.getKey())).findAny();
-        if (parserEntry.isPresent()) {
-            logger.debug("Identified parser {}", parserEntry.get().getValue().getClass().getSimpleName());
-            return parserEntry.get().getValue().parse(valueFromFile, context);
-        }
+        return PARSERS.entrySet()
+                .stream()
+                .filter(entry -> valueFromFile.startsWith(entry.getKey()))
+                .map(Map.Entry::getValue)
+                .findFirst()
+                .orElse(new SpringELParser())
+                .parse(this.sanitize(valueFromFile), context);
+    }
 
-        return valueFromFile;
+    /**
+     * Transforms various ways of describing the expressions like: ${request.value} which is equivalent to request.value
+     * or request#value which is equivalent to request.value.
+     *
+     * @param expression the expression as supplied by the user
+     * @return normalized form of the expression
+     */
+    private String sanitize(String expression) {
+        return expression.replaceAll("\\$\\{([^}]*)}", "$1")
+                .replaceAll("request#", "request.")
+                .replaceAll("\\$request", "request");
     }
 }
