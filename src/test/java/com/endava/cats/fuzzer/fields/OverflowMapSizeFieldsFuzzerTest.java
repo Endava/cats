da27affd16cf97d562db5c7f1dd2a5f6a7cc581a
Madalin Ilie <madalin.ilie@endava.com>
2023-06-26 20:17:53 +0300

    Add new Fuzzer to overflow map sizes

--- src/test/java/com/endava/cats/fuzzer/fields/OverflowMapSizeFieldsFuzzerTest.java
+++ src/test/java/com/endava/cats/fuzzer/fields/OverflowMapSizeFieldsFuzzerTest.java
@@ -14,7 +14,8 @@ import com.endava.cats.report.TestCaseListener;
 import com.endava.cats.util.CatsUtil;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectSpy;
-import io.swagger.v3.oas.models.media.ArraySchema;
+import io.swagger.v3.oas.models.media.MapSchema;
+import io.swagger.v3.oas.models.media.Schema;
 import jakarta.inject.Inject;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -28,14 +29,14 @@ import java.util.Map;
 import java.util.Set;
 
 @QuarkusTest
-class OverflowArraySizeFieldsFuzzerTest {
+class OverflowMapSizeFieldsFuzzerTest {
     ServiceCaller serviceCaller;
     @InjectSpy
     TestCaseListener testCaseListener;
     @InjectSpy
     CatsUtil catsUtil;
     FieldsIteratorExecutor catsExecutor;
-    private OverflowArraySizeFieldsFuzzer overflowArraySizeFieldsFuzzer;
+    private OverflowMapSizeFieldsFuzzer overflowMapSizeFieldsFuzzer;
 
     @Inject
     ProcessingArguments processingArguments;
@@ -45,22 +46,22 @@ class OverflowArraySizeFieldsFuzzerTest {
         serviceCaller = Mockito.mock(ServiceCaller.class);
         ReflectionTestUtils.setField(testCaseListener, "testCaseExporter", Mockito.mock(TestCaseExporter.class));
         catsExecutor = new FieldsIteratorExecutor(serviceCaller, testCaseListener, catsUtil, Mockito.mock(MatchArguments.class), Mockito.mock(FilesArguments.class));
-        overflowArraySizeFieldsFuzzer = new OverflowArraySizeFieldsFuzzer(catsExecutor, processingArguments);
+        overflowMapSizeFieldsFuzzer = new OverflowMapSizeFieldsFuzzer(catsExecutor, processingArguments);
     }
 
     @Test
     void shouldHaveDescription() {
-        Assertions.assertThat(overflowArraySizeFieldsFuzzer.description()).isNotBlank();
+        Assertions.assertThat(overflowMapSizeFieldsFuzzer.description()).isNotBlank();
     }
 
     @Test
     void shouldHaveToString() {
-        Assertions.assertThat(overflowArraySizeFieldsFuzzer).hasToString("OverflowArraySizeFieldsFuzzer");
+        Assertions.assertThat(overflowMapSizeFieldsFuzzer).hasToString("OverflowMapSizeFieldsFuzzer");
     }
 
     @Test
     void shouldSkipForNonBodyMethods() {
-        Assertions.assertThat(overflowArraySizeFieldsFuzzer.skipForHttpMethods())
+        Assertions.assertThat(overflowMapSizeFieldsFuzzer.skipForHttpMethods())
                 .contains(HttpMethod.GET, HttpMethod.DELETE, HttpMethod.HEAD);
     }
 
@@ -68,43 +69,63 @@ class OverflowArraySizeFieldsFuzzerTest {
     void shouldSkipIfFieldPrimitive() {
         FuzzingData data = Mockito.mock(FuzzingData.class);
         Mockito.when(data.getAllFieldsByHttpMethod()).thenReturn(Set.of("primitiveField"));
+        Mockito.when(data.getRequestPropertyTypes()).thenReturn(Map.of("primitiveField", new Schema()));
         Mockito.when(data.getPayload()).thenReturn("""
                     {"primitiveField": 3}
                 """);
-        overflowArraySizeFieldsFuzzer.fuzz(data);
+        overflowMapSizeFieldsFuzzer.fuzz(data);
         Mockito.verifyNoInteractions(testCaseListener);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"20", "null"}, nullValues = "null")
-    void shouldRunIfFieldArray(Integer maxItems) {
+    void shouldRunIfFieldDictionary(Integer maxItems) {
         FuzzingData data = Mockito.mock(FuzzingData.class);
-        Mockito.when(data.getAllFieldsByHttpMethod()).thenReturn(Set.of("arrayField"));
-        Mockito.when(data.getRequestPropertyTypes()).thenReturn(Map.of("arrayField", new ArraySchema().maxItems(maxItems)));
+        Schema mapSchema = new MapSchema().maxProperties(maxItems).additionalProperties(true);
+        Mockito.when(data.getAllFieldsByHttpMethod()).thenReturn(Set.of("mapField"));
+        Mockito.when(data.getRequestPropertyTypes()).thenReturn(Map.of("mapField", mapSchema));
         Mockito.when(data.getPayload()).thenReturn("""
-                   {"arrayField": [{
+                   {"mapField": {
                             "inner": "innerValue"
-                        }]
+                        }
                     }
                 """);
-        overflowArraySizeFieldsFuzzer.fuzz(data);
+        overflowMapSizeFieldsFuzzer.fuzz(data);
         Mockito.verify(testCaseListener, Mockito.times(1)).reportResult(Mockito.any(), Mockito.eq(data), Mockito.any(), Mockito.eq(ResponseCodeFamily.FOURXX));
+    }
+
+    @Test
+    void shouldSkipWhenMapFieldButNotFound() {
+        FuzzingData data = Mockito.mock(FuzzingData.class);
+        Schema mapSchema = new MapSchema().maxProperties(10).additionalProperties(true);
+        Mockito.when(data.getAllFieldsByHttpMethod()).thenReturn(Set.of("notFound"));
+        Mockito.when(data.getRequestPropertyTypes()).thenReturn(Map.of("notFound", mapSchema));
+        Mockito.when(data.getPayload()).thenReturn("""
+                   {"mapField": {
+                            "inner": "innerValue"
+                        }
+                    }
+                """);
+        overflowMapSizeFieldsFuzzer.fuzz(data);
+        Mockito.verifyNoInteractions(testCaseListener);
     }
 
 
     @Test
-    void shouldSkipIfFieldObjectAndNotArray() {
+    void shouldSkipIfFieldObjectAndNotDictionary() {
         FuzzingData data = Mockito.mock(FuzzingData.class);
         Mockito.when(serviceCaller.call(Mockito.any())).thenReturn(CatsResponse.builder().body("{}").responseCode(200).build());
 
         Mockito.when(data.getAllFieldsByHttpMethod()).thenReturn(Set.of("objectField"));
+        Mockito.when(data.getRequestPropertyTypes()).thenReturn(Map.of("objectField", new Schema()));
+
         Mockito.when(data.getPayload()).thenReturn("""
                     {"objectField": {
                             "inner": "innerValue"
                         }
                     }
                 """);
-        overflowArraySizeFieldsFuzzer.fuzz(data);
+        overflowMapSizeFieldsFuzzer.fuzz(data);
         Mockito.verifyNoInteractions(testCaseListener);
     }
 }
