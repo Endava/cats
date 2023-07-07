305574084546e1812452e8e0ee15abf8b8f74375
Madalin Ilie <madalin.ilie@endava.com>
2023-07-07 11:03:51 +0300

    Add possibility to select granular log levels

--- src/main/java/com/endava/cats/args/IgnoreArguments.java
+++ src/main/java/com/endava/cats/args/IgnoreArguments.java
@@ -2,11 +2,11 @@ package com.endava.cats.args;
 
 import com.endava.cats.http.ResponseCodeFamily;
 import com.endava.cats.model.CatsResponse;
+import jakarta.inject.Singleton;
 import lombok.Getter;
 import org.apache.commons.lang3.StringUtils;
 import picocli.CommandLine;
 
-import jakarta.inject.Singleton;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
