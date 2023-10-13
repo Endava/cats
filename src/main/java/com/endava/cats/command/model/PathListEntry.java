bf2509d803607f26da6a61888312c3f1524ee96b
Madalin Ilie <madalin.ilie@endava.com>
2023-10-13 08:53:44 +0300

    Add more details to the list --paths subcommand

--- src/main/java/com/endava/cats/command/model/PathListEntry.java
+++ src/main/java/com/endava/cats/command/model/PathListEntry.java
@@ -1,9 +1,28 @@
 package com.endava.cats.command.model;
 
+import com.endava.cats.http.HttpMethod;
 import lombok.Builder;
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 @Builder
+@Getter
 public class PathListEntry {
-    private String method;
-    private String path;
+    private int numberOfPaths;
+    private int numberOfOperations;
+    private List<PathDetails> pathDetailsList;
+
+    @Builder
+    @Getter
+    public static class PathDetails implements Comparable<PathDetails> {
+        private List<HttpMethod> methods;
+        private String path;
+        
+        @Override
+        public int compareTo(@NotNull PathListEntry.PathDetails o) {
+            return this.getPath().compareTo(o.getPath());
+        }
+    }
 }
