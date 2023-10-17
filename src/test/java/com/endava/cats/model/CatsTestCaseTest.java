68627cdd925b3daa26079a82cfe9e3e71cb05461
Madalin Ilie <madalin.ilie@endava.com>
2023-10-17 18:27:02 +0300

    Improve logging and reporting when tests are ignore based on --ignoreXXX arguments

--- src/test/java/com/endava/cats/model/CatsTestCaseTest.java
+++ src/test/java/com/endava/cats/model/CatsTestCaseTest.java
@@ -4,6 +4,8 @@ package com.endava.cats.model;
 import io.quarkus.test.junit.QuarkusTest;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 @QuarkusTest
 class CatsTestCaseTest {
@@ -27,6 +29,21 @@ class CatsTestCaseTest {
 
         Assertions.assertThat(catsTestCase.getFullRequestPath()).isEqualTo("http://example.com/orders");
         Assertions.assertThat(catsTestCase.getRequest().getUrl()).isEqualTo("http://example.com/orders");
+    }
+
+    @ParameterizedTest
+    @CsvSource({"skipped,false", "skip_reporting,false", "success,true", "other,true"})
+    void shouldReportSkip(String result, boolean skip) {
+        CatsTestCase catsTestCase = new CatsTestCase();
+        catsTestCase.setResult(result);
+        Assertions.assertThat(catsTestCase.isNotSkipped()).isEqualTo(skip);
+    }
+
+    @Test
+    void shouldSetSkip() {
+        CatsTestCase catsTestCase = new CatsTestCase();
+        catsTestCase.setResultSkipped();
+        Assertions.assertThat(catsTestCase.isNotSkipped()).isFalse();
     }
 
 }
