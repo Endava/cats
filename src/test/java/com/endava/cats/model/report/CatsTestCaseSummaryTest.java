029053391bcecea2821b13e643e1730b1aae9427
Madalin Ilie <madalin.ilie@endava.com>
2023-10-25 09:17:46 +0300

    Don't include request/response details in linters report file

--- src/test/java/com/endava/cats/model/report/CatsTestCaseSummaryTest.java
+++ src/test/java/com/endava/cats/model/report/CatsTestCaseSummaryTest.java
@@ -15,10 +15,11 @@ class CatsTestCaseSummaryTest {
     @Test
     void givenTwoTestCaseSummaryInstancesWithNoIntPart_whenComparingThem_thenTheyAreNotEqual() {
         CatsTestCase testCase1 = new CatsTestCase();
+        testCase1.setTestId("ID1");
         testCase1.setResponse(CatsResponse.empty());
         testCase1.setRequest(CatsRequest.builder().httpMethod("POST").build());
-        CatsTestCaseSummary summary1 = CatsTestCaseSummary.fromCatsTestCase("ID", testCase1);
-        CatsTestCaseSummary summary2 = CatsTestCaseSummary.fromCatsTestCase("ID", testCase1);
+        CatsTestCaseSummary summary1 = CatsTestCaseSummary.fromCatsTestCase(testCase1);
+        CatsTestCaseSummary summary2 = CatsTestCaseSummary.fromCatsTestCase(testCase1);
 
         Assertions.assertThat(summary1).isEqualByComparingTo(summary2);
     }
@@ -26,10 +27,14 @@ class CatsTestCaseSummaryTest {
     @Test
     void givenTwoTestCaseSummaryInstancesWithTheDifferentStringParts_whenComparingThem_thenTheyAreNotEqual() {
         CatsTestCase testCase1 = new CatsTestCase();
+        CatsTestCase testCase2 = new CatsTestCase();
+        testCase1.setTestId("ID1");
+        testCase2.setTestId("JD1");
+
         testCase1.setResponse(CatsResponse.empty());
         testCase1.setRequest(CatsRequest.builder().httpMethod("POST").build());
-        CatsTestCaseSummary summary1 = CatsTestCaseSummary.fromCatsTestCase("ID1", testCase1);
-        CatsTestCaseSummary summary2 = CatsTestCaseSummary.fromCatsTestCase("JD1", testCase1);
+        CatsTestCaseSummary summary1 = CatsTestCaseSummary.fromCatsTestCase(testCase1);
+        CatsTestCaseSummary summary2 = CatsTestCaseSummary.fromCatsTestCase(testCase2);
 
         Assertions.assertThat(summary1).isNotEqualByComparingTo(summary2);
     }
@@ -37,10 +42,11 @@ class CatsTestCaseSummaryTest {
     @Test
     void givenTwoTestCaseSummaryInstancesWithTheSameDetails_whenComparingThem_thenTheyAreEqual() {
         CatsTestCase testCase1 = new CatsTestCase();
+        testCase1.setTestId("ID1");
         testCase1.setResponse(CatsResponse.empty());
         testCase1.setRequest(CatsRequest.builder().httpMethod("POST").build());
-        CatsTestCaseSummary summary1 = CatsTestCaseSummary.fromCatsTestCase("ID1", testCase1);
-        CatsTestCaseSummary summary2 = CatsTestCaseSummary.fromCatsTestCase("ID1", testCase1);
+        CatsTestCaseSummary summary1 = CatsTestCaseSummary.fromCatsTestCase(testCase1);
+        CatsTestCaseSummary summary2 = CatsTestCaseSummary.fromCatsTestCase(testCase1);
 
         Assertions.assertThat(summary1).isEqualByComparingTo(summary2);
     }
@@ -48,10 +54,17 @@ class CatsTestCaseSummaryTest {
     @Test
     void givenTwoTestCaseSummaryInstancesWithTheDifferentDetails_whenComparingThem_thenTheyAreNotEqual() {
         CatsTestCase testCase1 = new CatsTestCase();
+        CatsTestCase testCase2 = new CatsTestCase();
+        testCase2.setTestId("ID2");
+        testCase1.setTestId("ID1");
+
         testCase1.setResponse(CatsResponse.empty());
         testCase1.setRequest(CatsRequest.builder().httpMethod("POST").build());
-        CatsTestCaseSummary summary1 = CatsTestCaseSummary.fromCatsTestCase("ID1", testCase1);
-        CatsTestCaseSummary summary2 = CatsTestCaseSummary.fromCatsTestCase("ID2", testCase1);
+
+        testCase2.setResponse(CatsResponse.empty());
+        testCase2.setRequest(CatsRequest.builder().httpMethod("POST").build());
+        CatsTestCaseSummary summary1 = CatsTestCaseSummary.fromCatsTestCase(testCase1);
+        CatsTestCaseSummary summary2 = CatsTestCaseSummary.fromCatsTestCase(testCase2);
 
         Assertions.assertThat(summary1).isNotEqualByComparingTo(summary2);
     }
