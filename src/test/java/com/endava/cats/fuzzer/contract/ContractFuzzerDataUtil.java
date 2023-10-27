42a66c2e1bc92c7f3ae445acd69f869ed3d73fe5
Madalin Ilie <madalin.ilie@endava.com>
2023-10-27 09:18:28 +0300

    Add additional unit tests

--- src/test/java/com/endava/cats/fuzzer/contract/ContractFuzzerDataUtil.java
+++ src/test/java/com/endava/cats/fuzzer/contract/ContractFuzzerDataUtil.java
@@ -5,6 +5,7 @@ import com.endava.cats.model.FuzzingData;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.media.StringSchema;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.responses.ApiResponses;
 
@@ -24,12 +25,15 @@ class ContractFuzzerDataUtil {
         for (String responseCode : responseCodes) {
             apiResponses.addApiResponse(responseCode, firstApiResponse);
         }
+        StringSchema firstName = new StringSchema();
+        StringSchema lastName = new StringSchema();
+        Map<String, Schema> properties = Map.of("firstName", firstName, "lastName", lastName);
         Map<String, Schema> schemaMap = new HashMap<>();
-        schemaMap.put(schemaName, new Schema().$ref(schemaName));
+        schemaMap.put(schemaName, new Schema().$ref(schemaName).properties(properties));
 
         operation.setResponses(apiResponses);
         pathItem.setPost(operation);
-        return FuzzingData.builder().path("/pets").method(method).pathItem(pathItem).reqSchemaName(schemaName)
+        return FuzzingData.builder().path("/pets").method(method).pathItem(pathItem).reqSchemaName(schemaName).requestPropertyTypes(properties)
                 .reqSchema(new Schema().$ref(schemaName)).schemaMap(schemaMap).responseCodes(Set.of(responseCodes)).headers(Set.of()).build();
     }
 
