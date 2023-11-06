78e703758e59d2271e4daa3a127a8ab1b31956bd
Madalin Ilie <madalin.ilie@endava.com>
2023-11-06 18:02:30 +0200

    Make --verbosity=summary the default value

---
+++ src/main/java/com/endava/cats/fuzzer/http/InsertEmptyArrayFuzzer.java
@@ -0,0 +1,30 @@
+package com.endava.cats.fuzzer.http;
+
+import jakarta.inject.Singleton;
+
+//@HttpFuzzer
+@Singleton
+public class InsertEmptyArrayFuzzer {
+//
+//    @Inject
+//    public InsertEmptyArrayFuzzer(SimpleExecutor executor) {
+//        super(executor);
+//    }
+//
+//    @Override
+//    protected String getScenario() {
+//        return "Send a request with an additional empty array element inside the body";
+//    }
+//
+//    @Override
+//    protected String getPayload(FuzzingData data) {
+//        String payload = data.getPayload();
+//        char firstChar = payload.charAt(0);
+//        return firstChar + "[], " + payload.substring(1);
+//    }
+//
+//    @Override
+//    public String description() {
+//        return "send a request with an additional empty array inside the body";
+//    }
+}
