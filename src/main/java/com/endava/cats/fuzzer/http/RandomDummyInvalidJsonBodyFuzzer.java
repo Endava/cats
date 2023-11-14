0925284fb06683c31d648986e30cbcdd48d3ba00
Madalin Ilie <madalin.ilie@endava.com>
2023-11-14 09:00:44 +0200

    Add new fuzzer for random dummy invalid json body

---
+++ src/main/java/com/endava/cats/fuzzer/http/RandomDummyInvalidJsonBodyFuzzer.java
@@ -0,0 +1,85 @@
+package com.endava.cats.fuzzer.http;
+
+import com.endava.cats.annotations.HttpFuzzer;
+import com.endava.cats.fuzzer.api.Fuzzer;
+import com.endava.cats.fuzzer.executor.SimpleExecutor;
+import com.endava.cats.fuzzer.executor.SimpleExecutorContext;
+import com.endava.cats.http.HttpMethod;
+import com.endava.cats.http.ResponseCodeFamily;
+import com.endava.cats.model.FuzzingData;
+import com.endava.cats.util.ConsoleUtils;
+import io.github.ludovicianul.prettylogger.PrettyLogger;
+import io.github.ludovicianul.prettylogger.PrettyLoggerFactory;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Singleton
+@HttpFuzzer
+public class RandomDummyInvalidJsonBodyFuzzer implements Fuzzer {
+    protected final PrettyLogger logger = PrettyLoggerFactory.getLogger(getClass());
+    private final SimpleExecutor simpleExecutor;
+    private static final List<String> PAYLOADS = List.of("{0}", "{0.0}", "[{}]", "{$}",
+            """ 
+                    {"circularRef": {"self": {"$ref": "#/circularRef"}}}
+                    """,
+            """
+                    {"backslash": "\\"}
+                     """,
+            """
+                    {"ünicode": "ünicode"}
+                    """,
+            """
+                    "{"unexpected" $ "token": "value"}
+                    """,
+            """
+                    {\u0000:\u0000}
+                    """,
+            """
+                    {"\u0000":"\u0000"}
+                    """,
+            """
+                    {"␀":"␀"}
+                    """,
+            """
+                    {␀:␀}
+                    """);
+
+    @Inject
+    RandomDummyInvalidJsonBodyFuzzer(SimpleExecutor ce) {
+        this.simpleExecutor = ce;
+    }
+
+    @Override
+    public void fuzz(FuzzingData data) {
+        for (String payload : PAYLOADS) {
+            simpleExecutor.execute(
+                    SimpleExecutorContext.builder()
+                            .expectedResponseCode(ResponseCodeFamily.FOURXX)
+                            .fuzzingData(data)
+                            .logger(logger)
+                            .replaceRefData(false)
+                            .scenario("Send %s as invalid json request body".formatted(payload))
+                            .fuzzer(this)
+                            .payload(payload)
+                            .build());
+        }
+    }
+
+    @Override
+    public String description() {
+        return "send a request with dummy invalid json body";
+    }
+
+    @Override
+    public String toString() {
+        return ConsoleUtils.sanitizeFuzzerName(this.getClass().getSimpleName());
+    }
+
+    @Override
+    public List<HttpMethod> skipForHttpMethods() {
+        return Arrays.asList(HttpMethod.GET, HttpMethod.DELETE);
+    }
+}
---
+++ src/test/java/com/endava/cats/fuzzer/http/RandomDummyInvalidJsonBodyFuzzerTest.java
@@ -0,0 +1,71 @@
+package com.endava.cats.fuzzer.http;
+
+import com.endava.cats.fuzzer.executor.SimpleExecutor;
+import com.endava.cats.http.HttpMethod;
+import com.endava.cats.http.ResponseCodeFamily;
+import com.endava.cats.io.ServiceCaller;
+import com.endava.cats.model.CatsResponse;
+import com.endava.cats.model.FuzzingData;
+import com.endava.cats.report.TestCaseExporter;
+import com.endava.cats.report.TestCaseListener;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectSpy;
+import io.swagger.v3.oas.models.media.StringSchema;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.List;
+
+@QuarkusTest
+class RandomDummyInvalidJsonBodyFuzzerTest {
+    private ServiceCaller serviceCaller;
+    @InjectSpy
+    private TestCaseListener testCaseListener;
+    private RandomDummyInvalidJsonBodyFuzzer randomDummyInvalidJsonBodyFuzzer;
+
+    @BeforeEach
+    void setup() {
+        serviceCaller = Mockito.mock(ServiceCaller.class);
+        SimpleExecutor simpleExecutor = new SimpleExecutor(testCaseListener, serviceCaller);
+        randomDummyInvalidJsonBodyFuzzer = new RandomDummyInvalidJsonBodyFuzzer(simpleExecutor);
+        ReflectionTestUtils.setField(testCaseListener, "testCaseExporter", Mockito.mock(TestCaseExporter.class));
+    }
+
+    @Test
+    void shouldRunForEmptyPayload() {
+        randomDummyInvalidJsonBodyFuzzer.fuzz(Mockito.mock(FuzzingData.class));
+
+        Mockito.verify(testCaseListener, Mockito.times(12)).reportResult(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.eq(ResponseCodeFamily.FOURXX), Mockito.anyBoolean());
+    }
+
+    @Test
+    void givenAHttpMethodWithPayload_whenApplyingTheMalformedJsonFuzzer_thenTheResultsAreCorrectlyReported() {
+        FuzzingData data = FuzzingData.builder().method(HttpMethod.POST).reqSchema(new StringSchema()).requestContentTypes(List.of("application/json")).build();
+        ReflectionTestUtils.setField(data, "processedPayload", "{\"id\": 1}");
+
+        CatsResponse catsResponse = CatsResponse.builder().body("{}").responseCode(400).build();
+        Mockito.when(serviceCaller.call(Mockito.any())).thenReturn(catsResponse);
+        Mockito.doNothing().when(testCaseListener).reportResult(Mockito.any(), Mockito.eq(data), Mockito.any(), Mockito.any(), Mockito.anyBoolean());
+
+        randomDummyInvalidJsonBodyFuzzer.fuzz(data);
+        Mockito.verify(testCaseListener, Mockito.times(12)).reportResult(Mockito.any(), Mockito.eq(data), Mockito.eq(catsResponse), Mockito.eq(ResponseCodeFamily.FOURXX), Mockito.anyBoolean());
+    }
+
+    @Test
+    void shouldHaveToString() {
+        Assertions.assertThat(randomDummyInvalidJsonBodyFuzzer).hasToString(randomDummyInvalidJsonBodyFuzzer.getClass().getSimpleName());
+    }
+
+    @Test
+    void shouldHaveDescription() {
+        Assertions.assertThat(randomDummyInvalidJsonBodyFuzzer.description()).isNotBlank();
+    }
+
+    @Test
+    void shouldSkipForNonHttpBodyMethods() {
+        Assertions.assertThat(randomDummyInvalidJsonBodyFuzzer.skipForHttpMethods()).contains(HttpMethod.GET, HttpMethod.DELETE);
+    }
+}
