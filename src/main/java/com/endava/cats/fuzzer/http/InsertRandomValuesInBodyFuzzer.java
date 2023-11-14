0dcedc85ee52859137d217f329fa63b55ce2247b
Madalin Ilie <madalin.ilie@endava.com>
2023-11-14 09:01:15 +0200

    Add new fuzzer that inserts invalid json elements in valid requests

--- src/main/java/com/endava/cats/fuzzer/http/InsertRandomValuesInBodyFuzzer.java
+++ src/main/java/com/endava/cats/fuzzer/http/InsertRandomValuesInBodyFuzzer.java
@@ -1,30 +1,92 @@
 package com.endava.cats.fuzzer.http;
 
+import com.endava.cats.annotations.HttpFuzzer;
+import com.endava.cats.fuzzer.api.Fuzzer;
+import com.endava.cats.fuzzer.executor.SimpleExecutor;
+import com.endava.cats.fuzzer.executor.SimpleExecutorContext;
+import com.endava.cats.http.HttpMethod;
+import com.endava.cats.http.ResponseCodeFamily;
+import com.endava.cats.json.JsonUtils;
+import com.endava.cats.model.FuzzingData;
+import com.endava.cats.util.ConsoleUtils;
+import io.github.ludovicianul.prettylogger.PrettyLogger;
+import io.github.ludovicianul.prettylogger.PrettyLoggerFactory;
+import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
-//@HttpFuzzer
+import java.util.Arrays;
+import java.util.List;
+
+@HttpFuzzer
 @Singleton
-public class InsertEmptyArrayFuzzer {
-//
-//    @Inject
-//    public InsertEmptyArrayFuzzer(SimpleExecutor executor) {
-//        super(executor);
-//    }
-//
-//    @Override
-//    protected String getScenario() {
-//        return "Send a request with an additional empty array element inside the body";
-//    }
-//
-//    @Override
-//    protected String getPayload(FuzzingData data) {
-//        String payload = data.getPayload();
-//        char firstChar = payload.charAt(0);
-//        return firstChar + "[], " + payload.substring(1);
-//    }
-//
-//    @Override
-//    public String description() {
-//        return "send a request with an additional empty array inside the body";
-//    }
+public class InsertRandomValuesInBodyFuzzer implements Fuzzer {
+
+    protected final PrettyLogger logger = PrettyLoggerFactory.getLogger(getClass());
+    private final SimpleExecutor simpleExecutor;
+    private static final List<String> PAYLOADS = List.of("{0},", "{0.0},", "[{}],", "{$},", "[]", "{}",
+            """ 
+                    {"circularRef": {"self": {"$ref": "#/circularRef"}}},
+                    """,
+            """
+                    {"backslash": "\\"},
+                     """,
+            """
+                    {"ünicode": "ünicode"},
+                    """,
+            """
+                    "{"unexpected" $ "token": "value"},
+                    """,
+            """
+                    {\u0000:\u0000},
+                    """,
+            """
+                    {"\u0000":"\u0000"},
+                    """,
+            """
+                    {"␀":"␀"},
+                    """,
+            """
+                    {␀:␀},
+                    """);
+
+    @Inject
+    InsertRandomValuesInBodyFuzzer(SimpleExecutor ce) {
+        this.simpleExecutor = ce;
+    }
+
+    @Override
+    public void fuzz(FuzzingData data) {
+        if (!JsonUtils.isEmptyPayload(data.getPayload())) {
+            for (String maliciousPayload : PAYLOADS) {
+                char firstChar = data.getPayload().charAt(0);
+                String finalPayload = firstChar + maliciousPayload + data.getPayload().substring(1);
+
+                simpleExecutor.execute(
+                        SimpleExecutorContext.builder()
+                                .expectedResponseCode(ResponseCodeFamily.FOURXX)
+                                .fuzzingData(data)
+                                .logger(logger)
+                                .replaceRefData(false)
+                                .scenario("Insert invalid data %s within a valid json request body".formatted(finalPayload))
+                                .fuzzer(this)
+                                .payload(finalPayload)
+                                .build());
+            }
+        }
+    }
+
+    @Override
+    public String description() {
+        return "insert invalid data within a valid request body";
+    }
+
+    @Override
+    public String toString() {
+        return ConsoleUtils.sanitizeFuzzerName(this.getClass().getSimpleName());
+    }
+
+    @Override
+    public List<HttpMethod> skipForHttpMethods() {
+        return Arrays.asList(HttpMethod.GET, HttpMethod.DELETE);
+    }
 }
---
+++ src/test/java/com/endava/cats/fuzzer/http/InsertRandomValuesInBodyFuzzerTest.java
@@ -0,0 +1,71 @@
+package com.endava.cats.fuzzer.http;
+
+import com.endava.cats.fuzzer.executor.SimpleExecutor;
+import com.endava.cats.http.HttpMethod;
+import com.endava.cats.http.ResponseCodeFamily;
+import com.endava.cats.io.ServiceCaller;
+import com.endava.cats.model.CatsResponse;
+import com.endava.cats.model.FuzzingData;
+import com.endava.cats.report.TestCaseExporter;
+import com.endava.cats.report.TestCaseListener;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectSpy;
+import io.swagger.v3.oas.models.media.StringSchema;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.List;
+
+@QuarkusTest
+class InsertRandomValuesInBodyFuzzerTest {
+    private ServiceCaller serviceCaller;
+    @InjectSpy
+    private TestCaseListener testCaseListener;
+    private InsertRandomValuesInBodyFuzzer insertRandomValuesInBodyFuzzer;
+
+    @BeforeEach
+    void setup() {
+        serviceCaller = Mockito.mock(ServiceCaller.class);
+        SimpleExecutor simpleExecutor = new SimpleExecutor(testCaseListener, serviceCaller);
+        insertRandomValuesInBodyFuzzer = new InsertRandomValuesInBodyFuzzer(simpleExecutor);
+        ReflectionTestUtils.setField(testCaseListener, "testCaseExporter", Mockito.mock(TestCaseExporter.class));
+    }
+
+    @Test
+    void shouldNotRunForEmptyPayload() {
+        insertRandomValuesInBodyFuzzer.fuzz(Mockito.mock(FuzzingData.class));
+
+        Mockito.verifyNoInteractions(testCaseListener);
+    }
+
+    @Test
+    void givenAHttpMethodWithPayload_whenApplyingTheMalformedJsonFuzzer_thenTheResultsAreCorrectlyReported() {
+        FuzzingData data = FuzzingData.builder().method(HttpMethod.POST).reqSchema(new StringSchema()).requestContentTypes(List.of("application/json")).build();
+        ReflectionTestUtils.setField(data, "processedPayload", "{\"id\": 1}");
+
+        CatsResponse catsResponse = CatsResponse.builder().body("{}").responseCode(400).build();
+        Mockito.when(serviceCaller.call(Mockito.any())).thenReturn(catsResponse);
+        Mockito.doNothing().when(testCaseListener).reportResult(Mockito.any(), Mockito.eq(data), Mockito.any(), Mockito.any(), Mockito.anyBoolean());
+
+        insertRandomValuesInBodyFuzzer.fuzz(data);
+        Mockito.verify(testCaseListener, Mockito.times(14)).reportResult(Mockito.any(), Mockito.eq(data), Mockito.eq(catsResponse), Mockito.eq(ResponseCodeFamily.FOURXX), Mockito.anyBoolean());
+    }
+
+    @Test
+    void shouldHaveToString() {
+        Assertions.assertThat(insertRandomValuesInBodyFuzzer).hasToString(insertRandomValuesInBodyFuzzer.getClass().getSimpleName());
+    }
+
+    @Test
+    void shouldHaveDescription() {
+        Assertions.assertThat(insertRandomValuesInBodyFuzzer.description()).isNotBlank();
+    }
+
+    @Test
+    void shouldSkipForNonHttpBodyMethods() {
+        Assertions.assertThat(insertRandomValuesInBodyFuzzer.skipForHttpMethods()).contains(HttpMethod.GET, HttpMethod.DELETE);
+    }
+}
