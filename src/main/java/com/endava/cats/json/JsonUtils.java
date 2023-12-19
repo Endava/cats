440e2c3444efbf33faa4f90748835f6aa84d0394
Madalin Ilie <madalin.ilie@endava.com>
2023-12-19 17:50:05 +0200

    Fix issue with keys having spaces in name

--- src/main/java/com/endava/cats/json/JsonUtils.java
+++ src/main/java/com/endava/cats/json/JsonUtils.java
@@ -29,11 +29,33 @@ import java.io.StringReader;
 import java.util.Set;
 import java.util.function.Predicate;
 
+/**
+ * Utility class for JSON objects interaction.
+ */
 public abstract class JsonUtils {
+    /**
+     * Used as a placeholder when a path is not found inside a given JSON
+     */
     public static final String NOT_SET = "NOT_SET";
+
+    /**
+     * Used to prefix the first json element from an array when dealing with JSON arrays.
+     */
     public static final String FIRST_ELEMENT_FROM_ROOT_ARRAY = "$[0]#";
+
+    /**
+     * Used to prefix array elements.
+     */
     public static final String ALL_ELEMENTS_ROOT_ARRAY = "$[*]#";
+
+    /**
+     * A permissive JSON parser.
+     */
     public static final JSONParser GENERIC_PERMISSIVE_PARSER = new JSONParser(JSONParser.MODE_PERMISSIVE);
+
+    /**
+     * A more strict JSON parser adhering to the RFC4627.
+     */
     public static final JSONParser JSON_STRICT_PARSER = new JSONParser(JSONParser.MODE_RFC4627);
 
     /**
@@ -59,24 +81,56 @@ public abstract class JsonUtils {
         //ntd
     }
 
+    /**
+     * Checks if the given value was not found when searched in a JSON using methods from this class.
+     *
+     * @param value the value to check
+     * @return true if the value was not found, false otherwise
+     */
     public static boolean isNotSet(String value) {
         return NOT_SET.equalsIgnoreCase(value);
     }
 
+
+    /**
+     * Replaces "#" with "." inside the given path.
+     *
+     * @param input the input path
+     * @return a path replacing "#"  with "."
+     */
     public static String sanitizeToJsonPath(String input) {
         return input.replace("#", ".");
     }
 
+    /**
+     * Checks if the 2 input strings are equal as JSON elements.
+     *
+     * @param json1 the first json
+     * @param json2 the second json
+     * @return true if the 2 inputs are the same as JSON elements, false otherwise
+     */
     public static boolean equalAsJson(String json1, String json2) {
         return JsonPath.parse(json1).jsonString().contentEquals(JsonPath.parse(json2).jsonString());
     }
 
+    /**
+     * Parses the given payload as a JsonElement.
+     *
+     * @param payload the given payload
+     * @return a JsonElement representing the given payload
+     */
     public static JsonElement parseAsJsonElement(String payload) {
         JsonReader reader = new JsonReader(new StringReader(payload));
         reader.setLenient(true);
         return JsonParser.parseReader(reader);
     }
 
+    /**
+     * Checks if the given string is a valid JSON.
+     *
+     * @param text the given text
+     * @return true if the input is a payload, false otherwise
+     */
     public static boolean isValidJson(String text) {
         try {
             JSON_STRICT_PARSER.parse(text);
@@ -169,7 +223,9 @@ public abstract class JsonUtils {
                 DocumentContext finalPayload = JsonPath.parse(interimPayload);
                 toEliminate.forEach(toEliminateKey -> {
                     try {
-                        finalPayload.delete(pathTowardsReplacement + "." + toEliminateKey);
+                        String nodeToDelete = getNodeKey(toEliminateKey, pathTowardsReplacement);
+                        LOGGER.debug("to delete {}", nodeToDelete);
+                        finalPayload.delete(JsonPath.compile(nodeToDelete).getPath());
                     } catch (PathNotFoundException ex) {
                         LOGGER.debug("Path not found when removing any_of/one_of: {}", ex.getMessage());
                     }
@@ -178,6 +234,22 @@ public abstract class JsonUtils {
             }
             return payload;
         }
+    }
+
+    /**
+     * Creates a node key based on the supplied strings in the form of {@code toEliminate.pathTowardsReplacement}.
+     * If any of the given input strings contains a space, it will enclose the final key in [''].
+     *
+     * @param toEliminateKey         start of the node key
+     * @param pathTowardsReplacement end of the node key
+     * @return a node key combining the given input
+     */
+    private static String getNodeKey(String toEliminateKey, String pathTowardsReplacement) {
+        String nodeToDelete = pathTowardsReplacement + "." + toEliminateKey;
+        if (nodeToDelete.contains(" ")) {
+            nodeToDelete = "['" + pathTowardsReplacement + "." + toEliminateKey + "']";
+        }
+        return nodeToDelete;
     }
 
     public static Object getVariableFromJson(String jsonPayload, String value) {
@@ -190,14 +262,34 @@ public abstract class JsonUtils {
         }
     }
 
+    /**
+     * Checks if the given field is present in the given json payload.
+     *
+     * @param jsonPayload the input payload
+     * @param field       the field to search
+     * @return true if the field is found inside the given payload, false otherwise
+     */
     public static boolean isFieldInJson(String jsonPayload, String field) {
         return !NOT_SET.equalsIgnoreCase(String.valueOf(getVariableFromJson(jsonPayload, field)));
     }
 
+    /**
+     * Checks if the given payload is null, empty or {}.
+     *
+     * @param payload the given payload
+     * @return true if the payload is empty, false otherwise
+     */
     public static boolean isEmptyPayload(String payload) {
         return payload == null || payload.isBlank() || payload.trim().equals("{}") || payload.trim().equals("\"{}\"");
     }
 
+    /**
+     * Determines if the current property is a cyclic reference to itself in the form of: prop#prop#prop#prop#... .
+     *
+     * @param currentProperty the property value
+     * @param depth           depth of cyclic search
+     * @return true if the given property is cyclic with the given depth, false otherwise
+     */
     public static boolean isCyclicReference(String currentProperty, int depth) {
         String[] properties = currentProperty.split("#", -1);
 
