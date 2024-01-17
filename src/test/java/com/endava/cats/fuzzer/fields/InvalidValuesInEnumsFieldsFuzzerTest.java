e76a8fd1fae6f9244143c662e5ce7865009f83c6
Madalin Ilie <madalin.ilie@endava.com>
2024-01-17 20:32:55 +0200

    Add new argument toogle for cases when services might allow invalid values in enums

--- src/test/java/com/endava/cats/fuzzer/fields/InvalidValuesInEnumsFieldsFuzzerTest.java
+++ src/test/java/com/endava/cats/fuzzer/fields/InvalidValuesInEnumsFieldsFuzzerTest.java
@@ -1,11 +1,14 @@
 package com.endava.cats.fuzzer.fields;
 
+import com.endava.cats.args.ProcessingArguments;
+import com.endava.cats.http.ResponseCodeFamily;
 import com.endava.cats.model.FuzzingData;
 import io.quarkus.test.junit.QuarkusTest;
 import io.swagger.v3.oas.models.media.StringSchema;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.Collections;
 
@@ -13,10 +16,12 @@ import java.util.Collections;
 class InvalidValuesInEnumsFieldsFuzzerTest {
 
     private InvalidValuesInEnumsFieldsFuzzer invalidValuesInEnumsFieldsFuzzer;
+    private ProcessingArguments processingArguments;
 
     @BeforeEach
     void setup() {
-        invalidValuesInEnumsFieldsFuzzer = new InvalidValuesInEnumsFieldsFuzzer(null, null, null, null);
+        processingArguments = Mockito.mock(ProcessingArguments.class);
+        invalidValuesInEnumsFieldsFuzzer = new InvalidValuesInEnumsFieldsFuzzer(null, null, null, null, processingArguments);
     }
 
     @Test
@@ -40,5 +45,20 @@ class InvalidValuesInEnumsFieldsFuzzerTest {
         Assertions.assertThat(invalidValuesInEnumsFieldsFuzzer.hasBoundaryDefined("test", data)).isTrue();
         Assertions.assertThat(invalidValuesInEnumsFieldsFuzzer.description()).isNotNull();
         Assertions.assertThat(invalidValuesInEnumsFieldsFuzzer.typeOfDataSentToTheService()).isNotNull();
+    }
+
+    @Test
+    void shouldReturn4XXIfNotAllowInvalidEnumValues() {
+        Assertions.assertThat(invalidValuesInEnumsFieldsFuzzer.getExpectedHttpCodeWhenFuzzedValueNotMatchesPattern()).isEqualTo(ResponseCodeFamily.FOURXX);
+        Assertions.assertThat(invalidValuesInEnumsFieldsFuzzer.getExpectedHttpCodeWhenOptionalFieldsAreFuzzed()).isEqualTo(ResponseCodeFamily.FOURXX);
+        Assertions.assertThat(invalidValuesInEnumsFieldsFuzzer.getExpectedHttpCodeWhenFuzzedValueNotMatchesPattern()).isEqualTo(ResponseCodeFamily.FOURXX);
+    }
+
+    @Test
+    void shouldReturn2XXIfAllowInvalidEnumValues() {
+        Mockito.when(processingArguments.isAllowInvalidEnumValues()).thenReturn(true);
+        Assertions.assertThat(invalidValuesInEnumsFieldsFuzzer.getExpectedHttpCodeWhenFuzzedValueNotMatchesPattern()).isEqualTo(ResponseCodeFamily.TWOXX);
+        Assertions.assertThat(invalidValuesInEnumsFieldsFuzzer.getExpectedHttpCodeWhenOptionalFieldsAreFuzzed()).isEqualTo(ResponseCodeFamily.TWOXX);
+        Assertions.assertThat(invalidValuesInEnumsFieldsFuzzer.getExpectedHttpCodeWhenFuzzedValueNotMatchesPattern()).isEqualTo(ResponseCodeFamily.TWOXX);
     }
 }
