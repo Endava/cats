410583c9f67757ed36afb6b9c8eeac433475047e
Madalin Ilie <madalin.ilie@endava.com>
2024-03-07 20:32:09 +0200

    Add new generators for address

---
+++ src/main/java/com/endava/cats/generator/format/impl/AddressGenerator.java
@@ -0,0 +1,37 @@
+package com.endava.cats.generator.format.impl;
+
+import com.endava.cats.generator.format.api.DataFormat;
+import com.endava.cats.generator.format.api.OpenAPIFormat;
+import com.endava.cats.generator.format.api.PropertySanitizer;
+import com.endava.cats.generator.format.api.ValidDataFormatGenerator;
+import com.endava.cats.util.CatsUtil;
+import io.swagger.v3.oas.models.media.Schema;
+import jakarta.inject.Singleton;
+
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Generates real addresses.
+ */
+@Singleton
+public class AddressGenerator implements ValidDataFormatGenerator, OpenAPIFormat {
+
+    @Override
+    public boolean appliesTo(String format, String propertyName) {
+        return "address".equalsIgnoreCase(PropertySanitizer.sanitize(format)) ||
+                PropertySanitizer.sanitize(propertyName).toLowerCase(Locale.ROOT).endsWith("address");
+    }
+
+    @Override
+    public List<String> matchingFormats() {
+        return List.of("address");
+    }
+
+    @Override
+    public Object generate(Schema<?> schema) {
+        String generated = CatsUtil.faker().address().fullAddress();
+
+        return DataFormat.matchesPatternOrNull(schema, generated);
+    }
+}
---
+++ src/test/java/com/endava/cats/generator/format/impl/AddressGeneratorTest.java
@@ -0,0 +1,32 @@
+package com.endava.cats.generator.format.impl;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.swagger.v3.oas.models.media.Schema;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+@QuarkusTest
+public class AddressGeneratorTest {
+
+    @Test
+    void shouldGenerate() {
+        AddressGenerator addressGenerator = new AddressGenerator();
+        Assertions.assertThat(addressGenerator.generate(new Schema<>()).toString()).hasSizeGreaterThan(5);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"address,true", "other,false"})
+    void shouldApplyToFormat(String format, boolean expected) {
+        AddressGenerator addressGenerator = new AddressGenerator();
+        Assertions.assertThat(addressGenerator.appliesTo(format, "")).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"address,true", "ADDRESS,true", "other,false"})
+    void shouldApplyToPropertyName(String property, boolean expected) {
+        AddressGenerator addressGenerator = new AddressGenerator();
+        Assertions.assertThat(addressGenerator.appliesTo("", property)).isEqualTo(expected);
+    }
+}
