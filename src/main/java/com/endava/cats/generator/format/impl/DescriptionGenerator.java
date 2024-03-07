bb1d8b29e9f7aafe8b2380ba637679bd1c1540cc
Madalin Ilie <madalin.ilie@endava.com>
2024-03-07 20:33:08 +0200

    Add new description generator

---
+++ src/main/java/com/endava/cats/generator/format/impl/DescriptionGenerator.java
@@ -0,0 +1,37 @@
+package com.endava.cats.generator.format.impl;
+
+import com.endava.cats.generator.format.api.DataFormat;
+import com.endava.cats.generator.format.api.OpenAPIFormat;
+import com.endava.cats.generator.format.api.PropertySanitizer;
+import com.endava.cats.generator.format.api.ValidDataFormatGenerator;
+import com.endava.cats.util.CatsUtil;
+import io.swagger.v3.oas.models.media.Schema;
+import jakarta.inject.Singleton;
+
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Generates quotes to fill in description fields.
+ */
+@Singleton
+public class DescriptionGenerator implements ValidDataFormatGenerator, OpenAPIFormat {
+
+    @Override
+    public boolean appliesTo(String format, String propertyName) {
+        return "description".equalsIgnoreCase(PropertySanitizer.sanitize(format)) ||
+                PropertySanitizer.sanitize(propertyName).toLowerCase(Locale.ROOT).endsWith("description");
+    }
+
+    @Override
+    public List<String> matchingFormats() {
+        return List.of("description");
+    }
+
+    @Override
+    public Object generate(Schema<?> schema) {
+        String generated = CatsUtil.faker().chuckNorris().fact();
+
+        return DataFormat.matchesPatternOrNull(schema, generated);
+    }
+}
