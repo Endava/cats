1db44f6441bef11f9d557473bf3cbccdfe0a4d5c
Madalin Ilie <madalin.ilie@endava.com>
2024-03-07 20:32:26 +0200

    Add new city generator

---
+++ src/main/java/com/endava/cats/generator/format/impl/CityGenerator.java
@@ -0,0 +1,38 @@
+package com.endava.cats.generator.format.impl;
+
+import com.endava.cats.generator.format.api.DataFormat;
+import com.endava.cats.generator.format.api.OpenAPIFormat;
+import com.endava.cats.generator.format.api.PropertySanitizer;
+import com.endava.cats.generator.format.api.ValidDataFormatGenerator;
+import com.endava.cats.util.CatsUtil;
+import io.swagger.v3.oas.models.media.Schema;
+import jakarta.inject.Singleton;
+
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Generates valid city names.
+ */
+@Singleton
+public class CityGenerator implements ValidDataFormatGenerator, OpenAPIFormat {
+
+    @Override
+    public boolean appliesTo(String format, String propertyName) {
+        return "city".equalsIgnoreCase(PropertySanitizer.sanitize(format)) ||
+                PropertySanitizer.sanitize(propertyName).toLowerCase(Locale.ROOT).endsWith("city") ||
+                PropertySanitizer.sanitize(propertyName).toLowerCase(Locale.ROOT).endsWith("cityname");
+    }
+
+    @Override
+    public List<String> matchingFormats() {
+        return List.of("city", "cityName", "city-name", "city_name");
+    }
+
+    @Override
+    public Object generate(Schema<?> schema) {
+        String generated = CatsUtil.faker().address().city();
+
+        return DataFormat.matchesPatternOrNull(schema, generated);
+    }
+}
---
+++ src/test/java/com/endava/cats/generator/format/impl/CityGeneratorTest.java
@@ -0,0 +1,31 @@
+package com.endava.cats.generator.format.impl;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.swagger.v3.oas.models.media.Schema;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+@QuarkusTest
+class CityGeneratorTest {
+    @Test
+    void shouldGenerate() {
+        CityGenerator cityGenerator = new CityGenerator();
+        Assertions.assertThat(cityGenerator.generate(new Schema<>()).toString()).hasSizeGreaterThan(5);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"city,true", "other,false"})
+    void shouldApplyToFormat(String format, boolean expected) {
+        CityGenerator cityGenerator = new CityGenerator();
+        Assertions.assertThat(cityGenerator.appliesTo(format, "")).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"city,true", "CITY,true", "cityName,true", "city_name,true", "other#city,true", "other, false"})
+            void shouldApplyToPropertyName(String property, boolean expected) {
+        CityGenerator cityGenerator = new CityGenerator();
+        Assertions.assertThat(cityGenerator.appliesTo("", property)).isEqualTo(expected);
+    }
+}
