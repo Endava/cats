ac22e2fd35350db0627001bc16ce605d5fa7f998
Madalin Ilie <madalin.ilie@endava.com>
2024-03-12 23:41:13 +0200

    Add new generator for cardholdername

---
+++ src/main/java/com/endava/cats/generator/format/impl/CardholderNameGenerator.java
@@ -0,0 +1,36 @@
+package com.endava.cats.generator.format.impl;
+
+import com.endava.cats.generator.format.api.DataFormat;
+import com.endava.cats.generator.format.api.OpenAPIFormat;
+import com.endava.cats.generator.format.api.PropertySanitizer;
+import com.endava.cats.generator.format.api.ValidDataFormatGenerator;
+import com.endava.cats.util.CatsUtil;
+import io.swagger.v3.oas.models.media.Schema;
+import jakarta.inject.Singleton;
+
+import java.util.List;
+
+/**
+ * Generates real world person names.
+ */
+@Singleton
+public class CardholderNameGenerator implements ValidDataFormatGenerator, OpenAPIFormat {
+
+    @Override
+    public boolean appliesTo(String format, String propertyName) {
+        return "personname".equalsIgnoreCase(PropertySanitizer.sanitize(format)) ||
+                PropertySanitizer.sanitize(propertyName).endsWith("cardholdername");
+    }
+
+    @Override
+    public List<String> matchingFormats() {
+        return List.of("cardHolderName", "card_holder_name", "card-holder-name", "");
+    }
+
+    @Override
+    public Object generate(Schema<?> schema) {
+        String generated = CatsUtil.faker().name().name();
+
+        return DataFormat.matchesPatternOrNull(schema, generated);
+    }
+}
