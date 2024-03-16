c35f3b2e6e924c0a0482da6ba9cffe1e81c09728
Madalin Ilie <madalin.ilie@endava.com>
2024-03-16 21:04:09 +0200

    Add new generator for business name

---
+++ src/main/java/com/endava/cats/generator/format/impl/BusinessNameGenerator.java
@@ -0,0 +1,41 @@
+package com.endava.cats.generator.format.impl;
+
+import com.endava.cats.generator.format.api.DataFormat;
+import com.endava.cats.generator.format.api.OpenAPIFormat;
+import com.endava.cats.generator.format.api.PropertySanitizer;
+import com.endava.cats.generator.format.api.ValidDataFormatGenerator;
+import com.endava.cats.util.CatsUtil;
+import io.swagger.v3.oas.models.media.Schema;
+import jakarta.inject.Singleton;
+
+import java.util.List;
+
+/**
+ * Generates real world company names.
+ */
+@Singleton
+public class BusinessNameGenerator implements ValidDataFormatGenerator, OpenAPIFormat {
+
+    @Override
+    public boolean appliesTo(String format, String propertyName) {
+        return "company".equalsIgnoreCase(PropertySanitizer.sanitize(format)) ||
+                PropertySanitizer.sanitize(propertyName).endsWith("company") ||
+                PropertySanitizer.sanitize(propertyName).endsWith("companyname") ||
+                PropertySanitizer.sanitize(propertyName).endsWith("businessname") ||
+                PropertySanitizer.sanitize(propertyName).endsWith("businesslegalname");
+    }
+
+    @Override
+    public List<String> matchingFormats() {
+        return List.of("company", "companyName", "company-name", "company_name",
+                "businessName", "business-name", "business_name",
+                "businessLegalName", "business-legal-name", "business_legal_name");
+    }
+
+    @Override
+    public Object generate(Schema<?> schema) {
+        String generated = CatsUtil.faker().company().name();
+
+        return DataFormat.matchesPatternOrNull(schema, generated);
+    }
+}
