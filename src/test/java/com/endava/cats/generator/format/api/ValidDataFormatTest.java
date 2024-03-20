568a241cd070a7b4c63adf1ec18c58ca96cbbdeb
Madalin Ilie <madalin.ilie@endava.com>
2024-03-20 22:29:32 +0200

    Add unit tests for missing cases

--- src/test/java/com/endava/cats/generator/format/api/ValidDataFormatTest.java
+++ src/test/java/com/endava/cats/generator/format/api/ValidDataFormatTest.java
@@ -4,7 +4,6 @@ import io.quarkus.test.junit.QuarkusTest;
 import io.swagger.v3.oas.models.media.Schema;
 import jakarta.inject.Inject;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -15,17 +14,22 @@ class ValidDataFormatTest {
     @Inject
     ValidDataFormat validDataFormat;
 
+
     @ParameterizedTest
-    @CsvSource({"city,true", "CITY,true", "cityName,true", "city_name,true", "other#city,true"})
+    @CsvSource({"city,true", "CITY,true", "cityName,true", "city_name,true", "other#city,true",
+            "iban,true", "IBAN,true", "other#iban,true"
+    })
     void shouldReturnOneGeneratorWithProperty(String propertyName) {
         List<ValidDataFormatGenerator> validDataFormatList = validDataFormat.getGenerators(new Schema<>(), propertyName);
         Assertions.assertThat(validDataFormatList).hasSize(1);
     }
 
-    @Test
-    void shouldReturnOneGeneratorWithFormat() {
+    @ParameterizedTest
+    @CsvSource({"city",
+            "iban"})
+    void shouldReturnOneGeneratorWithFormat(String format) {
         Schema<?> schema = new Schema<>();
-        schema.setFormat("city");
+        schema.setFormat(format);
         List<ValidDataFormatGenerator> validDataFormatList = validDataFormat.getGenerators(schema, "");
         Assertions.assertThat(validDataFormatList).hasSize(1);
     }
