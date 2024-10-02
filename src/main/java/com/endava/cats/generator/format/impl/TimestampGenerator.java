063b7d3fdcac6c6a7c528d3bb32c4f72abd5e14a
Madalin Ilie <madalin.ilie@endava.com>
2024-10-02 23:36:45 +0300

    feat: Add custom generator for timestamps

---
+++ src/main/java/com/endava/cats/generator/format/impl/TimestampGenerator.java
@@ -0,0 +1,47 @@
+package com.endava.cats.generator.format.impl;
+
+import com.endava.cats.generator.format.api.InvalidDataFormatGenerator;
+import com.endava.cats.generator.format.api.OpenAPIFormat;
+import com.endava.cats.generator.format.api.PropertySanitizer;
+import com.endava.cats.generator.format.api.ValidDataFormatGenerator;
+import io.swagger.v3.oas.models.media.Schema;
+import jakarta.inject.Singleton;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+/**
+ * A generator class implementing various interfaces for generating valid and invalid date-time data formats.
+ * It also implements the OpenAPIFormat interface.
+ */
+@Singleton
+public class TimestampGenerator implements ValidDataFormatGenerator, InvalidDataFormatGenerator, OpenAPIFormat {
+    @Override
+    public Object generate(Schema<?> schema) {
+        return ZonedDateTime.now(ZoneId.of("GMT")).format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+    }
+
+    @Override
+    public boolean appliesTo(String format, String propertyName) {
+        return PropertySanitizer.sanitize(propertyName).endsWith("timestamp") ||
+                "timestamp".equalsIgnoreCase(format);
+    }
+
+    @Override
+    public String getAlmostValidValue() {
+        return "2021-07-21-T10:22:1Z";
+    }
+
+    @Override
+    public String getTotallyWrongValue() {
+        return "1111-07-21T88:32:28Z";
+    }
+
+    @Override
+    public List<String> matchingFormats() {
+        return List.of("timestamp");
+    }
+}
+
