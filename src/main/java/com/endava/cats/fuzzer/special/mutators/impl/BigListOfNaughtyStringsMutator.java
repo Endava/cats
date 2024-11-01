c82635d86ad84f6b94a17cd33ab4dedfeeb1d72c
Madalin Ilie <madalin.ilie@endava.com>
2024-11-01 21:05:29 +0200

    feat: Add new mutator for big list of naughty strings

---
+++ src/main/java/com/endava/cats/fuzzer/special/mutators/impl/BigListOfNaughtyStringsMutator.java
@@ -0,0 +1,52 @@
+package com.endava.cats.fuzzer.special.mutators.impl;
+
+import com.endava.cats.fuzzer.special.mutators.api.BodyMutator;
+import com.endava.cats.util.CatsUtil;
+import com.endava.cats.util.JsonUtils;
+import jakarta.inject.Singleton;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * Sends random naughty strings in json fields.
+ */
+@Singleton
+public class BigListOfNaughtyStringsMutator implements BodyMutator {
+    private static final List<String> NAUGHTY_STRINGS;
+
+    static {
+        try (InputStream inputStream = BigListOfNaughtyStringsMutator.class.getClassLoader().getResourceAsStream("blns.txt");
+             BufferedReader reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(inputStream), StandardCharsets.UTF_8))) {
+
+            NAUGHTY_STRINGS = reader.lines()
+                    .filter(StringUtils::isNotBlank)
+                    .filter(Predicate.not(line -> line.startsWith("#")))
+                    .collect(Collectors.toList());
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to read BLNS resource file", e);
+        }
+    }
+
+    @Override
+    public String mutate(String inputJson, String selectedField) {
+        String randomNaughtyString = CatsUtil.selectRandom(NAUGHTY_STRINGS);
+        if (CatsUtil.random().nextBoolean()) {
+            randomNaughtyString = randomNaughtyString + JsonUtils.getVariableFromJson(inputJson, selectedField);
+        }
+        return CatsUtil.justReplaceField(inputJson, selectedField, randomNaughtyString).json();
+    }
+
+    @Override
+    public String description() {
+        return "replace field with random naughty strings";
+    }
+}
