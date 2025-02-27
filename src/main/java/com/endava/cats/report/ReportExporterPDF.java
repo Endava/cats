ec659b3296b5d187c7b0812f30ba2f9f6082b5f7
Juan Abreu <abreujuan1710study@gmail.com>
2025-02-27 16:50:40 +0100

    Fixes #153 - Adding a PDF export option

--- src/main/java/com/endava/cats/report/ReportExporterPDF.java
+++ src/main/java/com/endava/cats/report/ReportExporterPDF.java
@@ -1,33 +1,25 @@
 package com.endava.cats.report;
 
-import java.io.FileNotFoundException;
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
 
-import com.itextpdf.kernel.pdf.PdfDocument;
-import com.itextpdf.kernel.pdf.PdfWriter;
-import com.itextpdf.layout.Document;
-import com.itextpdf.layout.element.Paragraph;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ReportExporterPDF {
-    public void exportReportToPDF(String reportContent, String outputPath) {
-        try {
-            // Initialize PDF writer
-            PdfWriter writer = new PdfWriter(outputPath);
+    public void exportReportToPDF(String htmlFilePath, String outputPath) {
+        try (FileInputStream htmlFileStream = new FileInputStream(new File(htmlFilePath));
+             FileOutputStream pdfFileStream = new FileOutputStream(new File(outputPath))) {
 
-            // Initialize PDF document
-            PdfDocument pdf = new PdfDocument(writer);
-
-            // Initialize document
-            Document document = new Document(pdf);
-
-            // Add content to the document
-            document.add(new Paragraph(reportContent));
-
-            // Close document
-            document.close();
+            PdfRendererBuilder builder = new PdfRendererBuilder();
+            builder.withFile(new File(htmlFilePath));
+            builder.toStream(pdfFileStream);
+            builder.run();
 
             System.out.println("Report exported to PDF successfully.");
 
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
