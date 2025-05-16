cc78e69aeeeaa93cb8a49bdb921b5c4d7c52aae9
Madalin Ilie <madalin.ilie@endava.com>
2025-05-16 20:55:07 +0300

    feat: Add new linter to check operationId prefixes

---
+++ src/main/java/com/endava/cats/fuzzer/contract/OperationIdVerbPrefixLinterFuzzer.java
@@ -0,0 +1,63 @@
+package com.endava.cats.fuzzer.contract;
+
+import com.endava.cats.annotations.LinterFuzzer;
+import com.endava.cats.args.NamingArguments;
+import com.endava.cats.http.HttpMethod;
+import com.endava.cats.model.FuzzingData;
+import com.endava.cats.report.TestCaseListener;
+import io.github.ludovicianul.prettylogger.PrettyLogger;
+import io.github.ludovicianul.prettylogger.PrettyLoggerFactory;
+import io.swagger.v3.oas.models.Operation;
+import jakarta.inject.Singleton;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+@LinterFuzzer
+@Singleton
+public class OperationIdVerbPrefixLinterFuzzer extends BaseLinterFuzzer {
+    private final PrettyLogger log = PrettyLoggerFactory.getLogger(this.getClass());
+    private final NamingArguments namingArguments;
+
+    public OperationIdVerbPrefixLinterFuzzer(TestCaseListener tcl, NamingArguments namingArguments) {
+        super(tcl);
+        this.namingArguments = namingArguments;
+    }
+
+    @Override
+    public void process(FuzzingData data) {
+        Map<String, List<String>> prefixMappings = namingArguments.getOperationPrefixMappings();
+        List<String> allowedPrefixes = prefixMappings.get(data.getMethod().name().toLowerCase(Locale.ROOT));
+
+        testCaseListener.addScenario(log, "Check if the operationId starts with an allowed prefix: {}", allowedPrefixes);
+        testCaseListener.addExpectedResult(log, "OperationId begins with one of the allowed prefixes");
+
+        Operation operation = HttpMethod.getOperation(data.getMethod(), data.getPathItem());
+
+        String operationId = operation.getOperationId();
+
+        if (StringUtils.isBlank(operationId) || doesNotMatchAllowedPrefixes(allowedPrefixes, operationId)) {
+            testCaseListener.reportResultError(log, data, "OperationId prefix mismatch",
+                    "OperationId [{}] does not start with any allowed prefix {}", operationId, allowedPrefixes);
+        } else {
+            testCaseListener.reportResultInfo(log, data, "OperationId [{}] uses an allowed prefix", operationId);
+        }
+    }
+
+    private static boolean doesNotMatchAllowedPrefixes(List<String> allowedPrefixes, String operationId) {
+        return allowedPrefixes.stream()
+                .noneMatch(p -> operationId.toLowerCase(Locale.ROOT).startsWith(p.toLowerCase(Locale.ROOT)));
+    }
+
+    @Override
+    protected String runKey(FuzzingData data) {
+        return data.getPath() + data.getMethod();
+    }
+
+    @Override
+    public String description() {
+        return "verifies that each operationId starts with one of the allowed prefixes per HTTP method, based on configuration";
+    }
+}
---
+++ src/test/java/com/endava/cats/fuzzer/contract/OperationIdVerbPrefixLinterFuzzerTest.java
@@ -0,0 +1,84 @@
+package com.endava.cats.fuzzer.contract;
+
+import com.endava.cats.args.IgnoreArguments;
+import com.endava.cats.args.NamingArguments;
+import com.endava.cats.args.ReportingArguments;
+import com.endava.cats.context.CatsGlobalContext;
+import com.endava.cats.http.HttpMethod;
+import com.endava.cats.model.FuzzingData;
+import com.endava.cats.report.ExecutionStatisticsListener;
+import com.endava.cats.report.TestCaseExporter;
+import com.endava.cats.report.TestCaseExporterHtmlJs;
+import com.endava.cats.report.TestCaseListener;
+import io.quarkus.test.junit.QuarkusTest;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.PathItem;
+import jakarta.enterprise.inject.Instance;
+import jakarta.inject.Inject;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mockito;
+
+import java.util.stream.Stream;
+
+@QuarkusTest
+class OperationIdVerbPrefixLinterFuzzerTest {
+    private TestCaseListener testCaseListener;
+    private OperationIdVerbPrefixLinterFuzzer operationIdVerbPrefixLinterFuzzer;
+
+    @Inject
+    NamingArguments namingArguments;
+
+    @BeforeEach
+    void setup() {
+        Instance<TestCaseExporter> exporters = Mockito.mock(Instance.class);
+        TestCaseExporter exporter = Mockito.mock(TestCaseExporterHtmlJs.class);
+        Mockito.when(exporters.stream()).thenReturn(Stream.of(exporter));
+        testCaseListener = Mockito.spy(new TestCaseListener(Mockito.mock(CatsGlobalContext.class), Mockito.mock(ExecutionStatisticsListener.class), exporters,
+                Mockito.mock(IgnoreArguments.class), Mockito.mock(ReportingArguments.class)));
+        operationIdVerbPrefixLinterFuzzer = new OperationIdVerbPrefixLinterFuzzer(testCaseListener, namingArguments);
+        namingArguments.loadVerbMapFile();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"null", "createPeople", "deletePeople"}, nullValues = "null")
+    void shouldReportError(String operationId) throws Exception {
+        PathItem pathItem = Mockito.mock(PathItem.class);
+        Operation operation = new Operation();
+        operation.setOperationId(operationId);
+        Mockito.when(pathItem.getGet()).thenReturn(operation);
+
+        FuzzingData data = FuzzingData.builder().pathItem(pathItem).method(HttpMethod.GET).build();
+        operationIdVerbPrefixLinterFuzzer.fuzz(data);
+
+        Mockito.verify(testCaseListener, Mockito.times(1)).reportResultError(Mockito.any(), Mockito.any(), Mockito.eq("OperationId prefix mismatch"),
+                Mockito.eq("OperationId [{}] does not start with any allowed prefix {}"), Mockito.eq(operationId), Mockito.any());
+    }
+
+    @Test
+    void shouldReportInfo() throws Exception {
+        PathItem pathItem = Mockito.mock(PathItem.class);
+        Operation operation = new Operation();
+        operation.setOperationId("getPeople");
+        Mockito.when(pathItem.getGet()).thenReturn(operation);
+
+        FuzzingData data = FuzzingData.builder().pathItem(pathItem).method(HttpMethod.GET).build();
+        operationIdVerbPrefixLinterFuzzer.fuzz(data);
+
+        Mockito.verify(testCaseListener, Mockito.times(1)).reportResultInfo(Mockito.any(), Mockito.any(),
+                Mockito.eq("OperationId [{}] uses an allowed prefix"), Mockito.eq("getPeople"));
+    }
+
+    @Test
+    void shouldReturnSimpleClassNameForToString() {
+        Assertions.assertThat(operationIdVerbPrefixLinterFuzzer).hasToString(operationIdVerbPrefixLinterFuzzer.getClass().getSimpleName());
+    }
+
+    @Test
+    void shouldReturnMeaningfulDescription() {
+        Assertions.assertThat(operationIdVerbPrefixLinterFuzzer.description()).isEqualTo("verifies that each operationId starts with one of the allowed prefixes per HTTP method, based on configuration");
+    }
+}
