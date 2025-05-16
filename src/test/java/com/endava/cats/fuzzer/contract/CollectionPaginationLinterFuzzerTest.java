cd6f953a54e848f9409b4bf294602273638b7007
Madalin Ilie <madalin.ilie@endava.com>
2025-05-16 20:55:47 +0300

    feat: Add new linter to check pagination for get on collections

---
+++ src/test/java/com/endava/cats/fuzzer/contract/CollectionPaginationLinterFuzzerTest.java
@@ -0,0 +1,68 @@
+package com.endava.cats.fuzzer.contract;
+
+import com.endava.cats.args.IgnoreArguments;
+import com.endava.cats.args.ReportingArguments;
+import com.endava.cats.context.CatsGlobalContext;
+import com.endava.cats.model.FuzzingData;
+import com.endava.cats.report.ExecutionStatisticsListener;
+import com.endava.cats.report.TestCaseExporter;
+import com.endava.cats.report.TestCaseExporterHtmlJs;
+import com.endava.cats.report.TestCaseListener;
+import io.quarkus.test.junit.QuarkusTest;
+import io.swagger.parser.OpenAPIParser;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.media.Schema;
+import jakarta.enterprise.inject.Instance;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+@QuarkusTest
+class CollectionPaginationLinterFuzzerTest {
+    private TestCaseListener testCaseListener;
+    private CollectionPaginationLinterFuzzer collectionPaginationLinterFuzzer;
+
+    @BeforeEach
+    void setup() {
+        Instance<TestCaseExporter> exporters = Mockito.mock(Instance.class);
+        TestCaseExporter exporter = Mockito.mock(TestCaseExporterHtmlJs.class);
+        Mockito.when(exporters.stream()).thenReturn(Stream.of(exporter));
+        testCaseListener = Mockito.spy(new TestCaseListener(Mockito.mock(CatsGlobalContext.class), Mockito.mock(ExecutionStatisticsListener.class), exporters,
+                Mockito.mock(IgnoreArguments.class), Mockito.mock(ReportingArguments.class)));
+        collectionPaginationLinterFuzzer = new CollectionPaginationLinterFuzzer(testCaseListener);
+    }
+
+    @Test
+    void shouldReportWarn() throws Exception {
+        OpenAPI openAPI = new OpenAPIParser().readContents(Files.readString(Paths.get("src/test/resources/petstore31.yaml")), null, null).getOpenAPI();
+        FuzzingData data = FuzzingData.builder().openApi(openAPI).pathItem(openAPI.getPaths().get("/pets/operations-bad/{id}")).build();
+        collectionPaginationLinterFuzzer.fuzz(data);
+
+        Mockito.verify(testCaseListener, Mockito.times(1)).reportResultWarn(Mockito.any(), Mockito.any(), Mockito.eq("Missing pagination support on GET collection endpoints"),
+                Mockito.contains("GET on [/pet/findByTags] is missing pagination query parameters"));
+    }
+
+    @Test
+    void shouldReportInfo() throws Exception {
+        OpenAPI openAPI = new OpenAPIParser().readContents(Files.readString(Paths.get("src/test/resources/petstore.yml")), null, null).getOpenAPI();
+        FuzzingData data = FuzzingData.builder().openApi(openAPI).pathItem(openAPI.getPaths().get("/pets/operations/{id}")).reqSchema(new Schema()).build();
+        collectionPaginationLinterFuzzer.fuzz(data);
+
+        Mockito.verify(testCaseListener, Mockito.times(1)).reportResultInfo(Mockito.any(), Mockito.any(), Mockito.eq("All collection GET endpoints declare pagination support"));
+    }
+
+    @Test
+    void shouldReturnSimpleClassNameForToString() {
+        Assertions.assertThat(collectionPaginationLinterFuzzer).hasToString(collectionPaginationLinterFuzzer.getClass().getSimpleName());
+    }
+
+    @Test
+    void shouldReturnMeaningfulDescription() {
+        Assertions.assertThat(collectionPaginationLinterFuzzer.description()).isEqualTo("verifies that all GET operations on collection endpoints support pagination via query parameters like limit, offset, page, or cursor");
+    }
+}
