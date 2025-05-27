96284f338e2227c29a911c16a506b7a7d40238e2
Madalin Ilie <madalin.ilie@endava.com>
2025-05-27 22:23:42 +0300

    feat: Add new linter to check for unused components requestBodies

---
+++ src/main/java/com/endava/cats/fuzzer/contract/UnusedRequestBodiesLinter.java
@@ -0,0 +1,29 @@
+package com.endava.cats.fuzzer.contract;
+
+import com.endava.cats.annotations.Linter;
+import com.endava.cats.context.CatsGlobalContext;
+import com.endava.cats.fuzzer.contract.base.AbstractUnusedElementsLinter;
+import com.endava.cats.report.TestCaseListener;
+import io.swagger.v3.oas.models.Components;
+import jakarta.inject.Singleton;
+
+import java.util.Map;
+import java.util.function.Function;
+
+@Linter
+@Singleton
+public class UnusedRequestBodiesLinter extends AbstractUnusedElementsLinter {
+    public UnusedRequestBodiesLinter(TestCaseListener tcl, CatsGlobalContext catsGlobalContext) {
+        super(tcl, catsGlobalContext);
+    }
+
+    @Override
+    protected String getElementType() {
+        return "requestBodies";
+    }
+
+    @Override
+    protected Function<Components, Map<String, ?>> getElementsFunction() {
+        return Components::getRequestBodies;
+    }
+}
