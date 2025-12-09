7cc41c9f8853f8cc43e315c230ba9f40c00705f7
Madalin Ilie <madalin.ilie@endava.com>
2025-12-09 20:50:32 +0200

    feat: Add ascii logo and description in cats --version

--- src/main/java/com/endava/cats/util/VersionProvider.java
+++ src/main/java/com/endava/cats/util/VersionProvider.java
@@ -9,11 +9,45 @@ import java.util.Properties;
  */
 public class VersionProvider implements CommandLine.IVersionProvider {
 
+    private static final String LOGO = """
+            # # # # # # # # # # # # # # # # # # # # # # # # # #
+            #             _____   ___ _____ _____             #
+            #            /  __ \\ / _ \\_   _/  ___|            #
+            #            | /  \\// /_\\ \\| | \\ `--.             #
+            #            | |    |  _  || |  `--. \\            #
+            #            | \\__/\\| | | || | /\\__/ /            #
+            #             \\____/\\_| |_/\\_/ \\____/             #
+            #           .. ...    -.-. --- --- .-..           #
+            #                                                 #
+            # # # # # # # # # # # # # # # # # # # # # # # # # #
+            """;
+
+    private static final String DESCRIPTION = """
+            
+            @|bold CATS|@ - @|green C|@ontract @|green A|@PI @|green T|@esting and @|green S|@ecurity
+            
+            @|bold,underline OpenAPI fuzzer and negative testing tool for REST APIs.|@
+            
+            CATS automatically generates and runs thousands of test cases
+            to find bugs, security vulnerabilities, and contract violations
+            in your API without writing a single test.
+            
+            @|yellow GitHub:|@  https://github.com/Endava/cats
+            @|yellow Docs:|@    https://endava.github.io/cats/
+            """;
+
     @Override
     public String[] getVersion() throws Exception {
         Properties properties = new Properties();
         properties.load(getClass().getClassLoader().getResourceAsStream("version.properties"));
-        return new String[]{"%n@|green " + properties.getProperty("app.name") + " version " +
-                properties.getProperty("app.version") + "|@"};
+        String version = properties.getProperty("app.version");
+        String buildTime = properties.getProperty("app.buildTime");
+
+        return new String[]{
+                LOGO,
+                "@|bold,green " + properties.getProperty("app.name") + " version " + version + "|@",
+                "@|faint Built on: " + buildTime + "|@",
+                DESCRIPTION
+        };
     }
 }
